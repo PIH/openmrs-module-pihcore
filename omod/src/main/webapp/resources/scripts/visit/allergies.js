ff4bd7ceeea6ba0e361e54fb59b9a82df79a605e
Cosmin Ioan <cioan@pih.org>
2015-10-29 15:33:24 -0400

    UHM-2215, load full encounter when encounter's default state is the long form

--- omod/src/main/webapp/resources/scripts/visit/allergies.js
+++ omod/src/main/webapp/resources/scripts/visit/allergies.js
@@ -8,12 +8,25 @@ angular.module("allergies", [ "constants", "ngResource", "uicommons.common" ])
     .directive("currentAllergies", [ 'Allergies', function(Allergies) {
         return {
             scope: {
-                patient: "="
+                patient: "=",
+                visituuid: "="
             },
             controller: ["$scope", function($scope) {
                 if ($scope.patient) {
                     $scope.allergies = Allergies.get({uuid: $scope.patient.uuid});
                 }
+
+                $scope.showAlergiesDetails = false;
+                $scope.expandAllergies = function(showAlergiesDetails) {
+                    $scope.showAlergiesDetails = !showAlergiesDetails;
+                }
+                $scope.goToPage = function(provider, page, opts) {
+                    if (opts['returnUrl'] === undefined) {
+                        opts['returnUrl'] = "/" + OPENMRS_CONTEXT_PATH + "/pihcore/visit/visit.page?visit=" + $scope.visituuid;
+                    }
+                    location.href = emr.pageLink(provider, page, opts);
+                }
+
             }],
             templateUrl: "templates/allergiesList.page"
         }
