f603c7faddf71a66f1a9b9a359862acaf1979ca1
mogoodrich <mgoodrich@pih.org>
2015-11-05 09:56:45 -0500

    UHM-2085: Configure address hierarchy for Sierra Leone

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneAddressBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneAddressBundle.java
@@ -13,14 +13,14 @@ public class SierraLeoneAddressBundle extends AddressBundle {
 
     @Override
     public int getVersion() {
-        return 1;
+        return 2;
     }
 
     @Override
     public List<AddressComponent> getAddressComponents() {
         List<AddressComponent> l = new ArrayList<AddressComponent>();
         l.add(new AddressComponent(AddressField.COUNTRY, "Country", 40, "Sierra Leone", true));
-        l.add(new AddressComponent(AddressField.STATE_PROVINCE, "Province/Area", 40, null, false));
+        l.add(new AddressComponent(AddressField.STATE_PROVINCE, "Province/Area", 40, null, true));
         l.add(new AddressComponent(AddressField.COUNTY_DISTRICT, "District", 40, null, false));
         l.add(new AddressComponent(AddressField.CITY_VILLAGE, "Chiefdom", 40, null, false));
         l.add(new AddressComponent(AddressField.ADDRESS_1, "Address", 80, null, false));
