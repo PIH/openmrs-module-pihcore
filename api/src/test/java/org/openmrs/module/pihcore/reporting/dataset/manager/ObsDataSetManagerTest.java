a65e8c2b7c50e2e80e10bd0bd28a59033e4f2f77
mogoodrich <mgoodrich@pih.org>
2016-05-23 17:09:48 -0400

    ignore incomplete diagnosis data set test

--- api/src/test/java/org/openmrs/module/pihcore/reporting/dataset/manager/ObsDataSetManagerTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/reporting/dataset/manager/ObsDataSetManagerTest.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Locale;
 
-public class ObsDataSetManagerTest extends BaseReportTest {
+public abstract class ObsDataSetManagerTest extends BaseReportTest {
 
     @Autowired
     protected DataSetDefinitionService dataSetDefinitionService;
