aef3fc96f8df46b26f0ece22ca5e38d943b6cee2
mgoodrich <mark.goodrich@live.com>
2017-06-23 15:48:16 -0400

    UHM-2905: Create HIV metadata for program, workflow, state, and outcomes Fixed HIV program concepts

--- api/src/main/java/org/openmrs/module/pihcore/metadata/haiti/mirebalais/PihHaitiPrograms.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/haiti/mirebalais/PihHaitiPrograms.java
@@ -2,6 +2,8 @@ package org.openmrs.module.pihcore.metadata.haiti.mirebalais;
 
 import org.openmrs.module.metadatadeploy.descriptor.ProgramDescriptor;
 
+import static org.openmrs.module.pihcore.PihCoreConstants.HIV_PROGRAM_CONCEPT_UUID;
+import static org.openmrs.module.pihcore.PihCoreConstants.HIV_PROGRAM_OUTCOMES_CONCEPT_UUID;
 import static org.openmrs.module.pihcore.PihCoreConstants.ZIKA_PROGRAM_CONCEPT_UUID;
 import static org.openmrs.module.pihcore.PihCoreConstants.ZIKA_PROGRAM_OUTCOMES_CONCEPT_UUID;
 
@@ -16,10 +18,10 @@ public class PihHaitiPrograms {
     };
 
     public static ProgramDescriptor HIV = new ProgramDescriptor() {
-        public String conceptUuid() { return ZIKA_PROGRAM_CONCEPT_UUID; }   // this concept is installed via metadata package
+        public String conceptUuid() { return HIV_PROGRAM_CONCEPT_UUID; }   // this concept is installed via metadata package
         public String name() { return "HIV"; }
         public String description() { return "HIV Program"; }
-        @Override public String outcomesConceptUuid()  { return ZIKA_PROGRAM_OUTCOMES_CONCEPT_UUID; }   // this concept is installed via metadata package
+        @Override public String outcomesConceptUuid()  { return HIV_PROGRAM_OUTCOMES_CONCEPT_UUID; }   // this concept is installed via metadata package
         public String uuid() { return "b1cb1fc1-5190-4f7a-af08-48870975dafc"; }
     };
 
