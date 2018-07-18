5b0869b9e6847f1fee8505565e48b7831a519f6f
lnball <eball@pih.org>
2018-07-18 13:27:21 -0400

    UHM-3427 MCH provider role and relationships

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/ProviderRoleBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/ProviderRoleBundle.java
@@ -31,6 +31,7 @@ public class ProviderRoleBundle extends AbstractMetadataBundle {
     public static final class ProviderRoles {
         public static final String CHW = "68624C4C-9E10-473B-A849-204820D16C45";
         public static final String CHW_SUPERVISOR = "11C1A56D-82F7-4269-95E8-2B67B9A3D837";
+        public static final String NURSE_ACCOMPAGNATEUR = "9a4b44b2-8a9f-11e8-9a94-a6cf71072f73";
     }
 
     /**
@@ -43,9 +44,16 @@ public class ProviderRoleBundle extends AbstractMetadataBundle {
 
         Set<RelationshipType> relationshipTypes = null;
         RelationshipType vhwToPatient = personService.getRelationshipTypeByUuid(RelationshipTypeBundle.RelationshipTypes.CHW_TO_PATIENT);
+        RelationshipType nurseVhwToPatient = personService.getRelationshipTypeByUuid(RelationshipTypeBundle.RelationshipTypes.NURSE_CHW_PATIENT);
+
         if (vhwToPatient != null) {
             relationshipTypes = new HashSet<RelationshipType>();
             relationshipTypes.add(vhwToPatient);
+        }
+
+        if (nurseVhwToPatient != null) {
+            relationshipTypes = new HashSet<RelationshipType>();
+            relationshipTypes.add(nurseVhwToPatient);
         }
 
         Set<ProviderAttributeType> providerAttributes = new HashSet<ProviderAttributeType>();
@@ -59,6 +67,7 @@ public class ProviderRoleBundle extends AbstractMetadataBundle {
         }
 
         ProviderRole vhwSupervisee = install(providerRole("CHW", null, relationshipTypes, providerAttributes, ProviderRoles.CHW));
+        ProviderRole nurseAccompagnateur = install(providerRole("Nurse Accompagnateur", null, relationshipTypes, providerAttributes, ProviderRoles.NURSE_ACCOMPAGNATEUR));
 
         Set<ProviderRole> superviseeRoles = null;
         if (vhwSupervisee != null ) {
@@ -67,8 +76,6 @@ public class ProviderRoleBundle extends AbstractMetadataBundle {
         }
 
         install(providerRole("CHW Supervisor", superviseeRoles, relationshipTypes, providerAttributes, ProviderRoles.CHW_SUPERVISOR));
-
-
     }
 
 }
