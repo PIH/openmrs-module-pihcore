59281b499d0fe5ba67640114f5c3375fb4dc6c50
lnball <eball@pih.org>
2018-10-05 20:06:01 -0400

    UHM-3524,3516,3530,3536,3529 Lab result encounter date/estimated date, physical exam, ncd kidney+type of diabetes, J9 dangers

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/program/OncologyProgramBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/program/OncologyProgramBundle.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.pihcore.deploy.bundle.core.program;
 
-import org.openmrs.ProgramWorkflow;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
 import org.openmrs.module.pihcore.metadata.core.program.OncologyProgram;
 import org.springframework.stereotype.Component;
@@ -10,8 +9,6 @@ public class OncologyProgramBundle extends AbstractMetadataBundle {
 
     @Override
     public void install() throws Exception {
-        // ToDo: Clear out the old unused states
-        // uninstall(possible(ProgramWorkflow.class, OncologyProgram.ONCOLOGY_PROGRESS_STATUS.uuid()), "refresh");
         install(OncologyProgram.ONCOLOGY);
     }
 }
