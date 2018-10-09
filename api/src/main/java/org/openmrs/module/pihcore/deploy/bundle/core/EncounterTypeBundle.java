420248dda25e8c37d546f307c3d2a8bab3382ecc
Brandon Istenes <bistenes@gmail.com>
2018-10-09 16:46:10 -0500

    Get rid of initial visit form

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/EncounterTypeBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/EncounterTypeBundle.java
@@ -46,8 +46,7 @@ public class EncounterTypeBundle extends AbstractMetadataBundle {
         install(EncounterTypes.ANC_INTAKE);
         install(EncounterTypes.ANC_FOLLOWUP);
         install(EncounterTypes.MCH_DELIVERY);
-        install(EncounterTypes.MEXICO_CLINIC_INITIAL_VISIT);
-        install(EncounterTypes.MEXICO_CLINIC_FOLLOWUP_VISIT);
+        install(EncounterTypes.MEXICO_CLINIC_VISIT);
 
         uninstall(possible(EncounterType.class, AttachmentsConstants.ENCOUNTER_TYPE_UUID), "not used");
         uninstall(possible(EncounterType.class, EncounterTypes.PRIMARY_CARE_DISPOSITION.uuid()), "never used");
