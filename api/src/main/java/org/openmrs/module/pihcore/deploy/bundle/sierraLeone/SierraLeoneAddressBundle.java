9672d6a0f1d8cd6bf456ae75115f270ec31ebfb6
lnball <eball@pih.org>
2019-03-05 16:40:28 -0500

    SL-77 Swap address fields -- section and village

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneAddressBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneAddressBundle.java
@@ -13,7 +13,7 @@ public class SierraLeoneAddressBundle extends AddressBundle {
 
     @Override
     public int getVersion() {
-        return 7;
+        return 8;
     }
 
     @Override
@@ -22,8 +22,8 @@ public class SierraLeoneAddressBundle extends AddressBundle {
         l.add(new AddressComponent(AddressField.COUNTRY, "Country", 40, "Sierra Leone", true));
         l.add(new AddressComponent(AddressField.COUNTY_DISTRICT, "District", 40, null, true));
         l.add(new AddressComponent(AddressField.STATE_PROVINCE, "Chiefdom", 40, null, true));
-        l.add(new AddressComponent(AddressField.CITY_VILLAGE, "Section", 40, null, false));
-        l.add(new AddressComponent(AddressField.ADDRESS_1, "Village", 40, null, false));
+        l.add(new AddressComponent(AddressField.ADDRESS_1, "Section", 40, null, false));
+        l.add(new AddressComponent(AddressField.CITY_VILLAGE, "Village", 40, null, false));
         l.add(new AddressComponent(AddressField.ADDRESS_2, "Address", 80, null, false));
         return l;
     }
@@ -32,8 +32,8 @@ public class SierraLeoneAddressBundle extends AddressBundle {
     public List<String> getLineByLineFormat() {
         List<String> l = new ArrayList<String>();
         l.add("address2");
-        l.add("address1");
         l.add("cityVillage");
+        l.add("address1");
         l.add("stateProvince");
         l.add("countyDistrict");
         l.add("country");
