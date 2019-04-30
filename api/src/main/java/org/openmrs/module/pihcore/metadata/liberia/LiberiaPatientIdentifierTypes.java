c2255456206868b09a0be8d4537a985175f1f898
Amos Laboso <amos.laboso@gmail.com>
2019-04-30 14:25:02 +0000

    LIB:  Added JJ Dossen as site/facility supported and updated patient … (#48)
    * LIB:  Added JJ Dossen as site/facility supported and updated patient identifier and identifier types & sources
    
    * LIB: Location fix, JJ_Dossen and Kouka use the same location Harper, with configs on json configs

--- api/src/main/java/org/openmrs/module/pihcore/metadata/liberia/LiberiaPatientIdentifierTypes.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/liberia/LiberiaPatientIdentifierTypes.java
@@ -5,10 +5,10 @@ import org.openmrs.patient.IdentifierValidator;
 
 public class LiberiaPatientIdentifierTypes {
 
-    public static PatientIdentifierTypeDescriptor PLEEBO_EMR_ID = new PatientIdentifierTypeDescriptor() {
+    public static PatientIdentifierTypeDescriptor LIBERIA_EMR_ID = new PatientIdentifierTypeDescriptor() {
         public String uuid() { return "0bc545e0-f401-11e4-b939-0800200c9a66"; }
-        public String name() { return "Pleebo EMR ID"; }
-        public String description() { return "A unique identifier issued to all patients in the Pleebo EMR." ; }
+        public String name() { return "Liberia EMR ID"; }
+        public String description() { return "A unique identifier issued to all patients in the Liberia EMR." ; }
         public Class<? extends IdentifierValidator> validator() { return null; }
     };
 
