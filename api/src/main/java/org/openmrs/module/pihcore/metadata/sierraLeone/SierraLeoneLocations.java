0fce277bb55f00b9e4344a3df6f787a7484e844d
mgoodrich <mark.goodrich@live.com>
2019-06-27 15:24:47 -0400

    Change name of Wellbody Location

--- api/src/main/java/org/openmrs/module/pihcore/metadata/sierraLeone/SierraLeoneLocations.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/sierraLeone/SierraLeoneLocations.java
@@ -11,8 +11,8 @@ public class SierraLeoneLocations {
 
     public static LocationDescriptor WELLBODY_HEALTH_CENTER = new LocationDescriptor() {
         public String uuid() { return "b6733150-7426-11e5-a837-0800200c9a66"; }
-        public String name() { return "Wellbody Health Center"; }
-        public String description() { return "Wellbody Health Center"; }
+        public String name() { return "Wellbody Clinic"; }
+        public String description() { return "Wellbody Clinic"; }
         public List<LocationTagDescriptor> tags() { return Arrays.asList(LocationTags.MEDICAL_RECORD_LOCATION, LocationTags.ARCHIVES_LOCATION, LocationTags.VISIT_LOCATION); }
     };
 }
