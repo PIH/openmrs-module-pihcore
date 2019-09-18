8a66b63c2734e03259997081969abc46e14f87d4
mgoodrich <mark.goodrich@live.com>
2019-09-18 15:09:28 -0400

    install MCH Programs in Sierra Leone

---
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneMetadataToInstallAfterConceptsBundle.java
@@ -0,0 +1,20 @@
+package org.openmrs.module.pihcore.deploy.bundle.sierraLeone;
+
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.openmrs.module.metadatadeploy.bundle.Requires;
+import org.openmrs.module.pihcore.deploy.bundle.core.program.MCHProgramBundle;
+import org.springframework.stereotype.Component;
+
+// this is a separate bundle that needs to be installed via the Mirebalais Metadata module *after*
+// concepts have been installed via metadata sharing (since programs reference concepts)
+// TODO: incorporate all PIH metadata installation into this module, remove Mirebalais Metadata module?
+@Component
+@Requires( {
+        MCHProgramBundle.class
+} )
+public class SierraLeoneMetadataToInstallAfterConceptsBundle extends AbstractMetadataBundle {
+    @Override
+    public void install() throws Exception {
+
+    }
+}
