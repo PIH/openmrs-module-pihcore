88c7c2292db73bfeb9f6a81b8a4b1b4684a3516e
Amos Laboso <alaboso@pih.org>
2019-10-02 13:03:32 +0000

    LIB - New functionality to incorporate regex for phoneNumber field in registration config

--- api/src/main/java/org/openmrs/module/pihcore/config/registration/RegistrationFieldDescriptor.java
+++ api/src/main/java/org/openmrs/module/pihcore/config/registration/RegistrationFieldDescriptor.java
@@ -7,11 +7,22 @@ public class RegistrationFieldDescriptor {
     @JsonProperty
     private Boolean required;
 
+    @JsonProperty
+    private String regex;
+
     public Boolean getRequired() {
         return required;
     }
 
     public void setRequired(Boolean required) {
         this.required = required;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
     }
 }
