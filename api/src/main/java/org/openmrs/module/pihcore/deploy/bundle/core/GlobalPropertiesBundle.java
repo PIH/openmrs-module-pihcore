a07f3117f48b7d7a3e55329c18de92a7ba26efee
Cosmin Ioan <cioan@pih.org>
2019-10-04 10:44:39 -0400

    UHM-4033, configure attachments encounter type GP

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/GlobalPropertiesBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/GlobalPropertiesBundle.java
@@ -127,7 +127,7 @@ public class GlobalPropertiesBundle extends AbstractMetadataBundle {
         properties.put(AttachmentsConstants.GP_WEBCAM_ALLOWED, "false");
         properties.put(AttachmentsConstants.GP_MAX_STORAGE_FILE_SIZE, "5.0");
         properties.put(AttachmentsConstants.GP_MAX_UPLOAD_FILE_SIZE, "5.0");
-        properties.put(AttachmentsConstants.GP_ENCOUNTER_TYPE_UUID, "");
+        properties.put(AttachmentsConstants.GP_ENCOUNTER_TYPE_UUID, "5021b1a1-e7f6-44b4-ba02-da2f2bcf8718"); //Visit Document Upload Encounter Type
 
         // Order Entry OWA
         // TODO: can we get rid of "order.encounterType" and "order.encounterRole" GP?
