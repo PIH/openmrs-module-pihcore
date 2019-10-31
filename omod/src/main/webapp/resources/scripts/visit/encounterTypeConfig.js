eb9b64cf37fc267ed07f709f211cb0d21336d6e7
James Mbabazi <12864648+jmbabazi@users.noreply.github.com>
2019-10-31 17:05:45 -0400

    SL-130 Adding prescribing ability to MCH screens (#123)

--- omod/src/main/webapp/resources/scripts/visit/encounterTypeConfig.js
+++ omod/src/main/webapp/resources/scripts/visit/encounterTypeConfig.js
@@ -125,6 +125,21 @@ angular.module("encounterTypeConfig", ["constants"])
 
         };
 
+        //SierraLeone
+          var primaryCarePlanMedication = {
+          type: "encounter-section",
+          id: "pihcore-plan-medication",
+          label: "pihcore.visitNote.plan",
+          icon: "icon-list-ul",
+          shortTemplate: "templates/sections/primaryCarePlanSectionShort.page",
+          longTemplate: "templates/sections/viewPlanSectionWithHtmlFormLong.page",
+          printTemplate: "templates/sections/printPrescriptionsWithHtmlFormLong.page",
+          printTemplateUrl: "/htmlformentryui/htmlform/viewEncounterWithHtmlForm/getAsHtml.action?encounterId={{encounter.uuid}}&definitionUiResource=pihcore:htmlforms/section-prescriptions-print.xml",
+          templateModelUrl: "/htmlformentryui/htmlform/viewEncounterWithHtmlForm/getAsHtml.action?encounterId={{encounter.uuid}}&definitionUiResource=pihcore:htmlforms/section-plan-medication.xml",
+          editUrl: "/htmlformentryui/htmlform/editHtmlFormWithStandardUi.page?patientId={{visit.patient.uuid}}&visitId={{visit.uuid}}&encounterId={{encounter.uuid}}&definitionUiResource=pihcore:htmlforms/section-plan-medication.xml&returnUrl={{returnUrl}}&breadcrumbOverride={{breadcrumbOverride}}"
+
+        };
+
         var ncd = {
             type: "encounter-section",
             id: "pihcore-ncd",
@@ -754,7 +769,8 @@ angular.module("encounterTypeConfig", ["constants"])
             sections: [
               cloneSectionAndUpdateCountryPathInUrls(ancInitial, "sierra_leone"),
               ancVaccinations,
-              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone")
+              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone"),
+              cloneSectionAndUpdateCountryPathInUrls(primaryCarePlanMedication, "sierra_leone")
             ]
           }
         };
@@ -792,7 +808,8 @@ angular.module("encounterTypeConfig", ["constants"])
             sections: [
               cloneSectionAndUpdateCountryPathInUrls(ancFollowup, "sierra_leone"),
               ancVaccinations,
-              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone")
+              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone"),
+              cloneSectionAndUpdateCountryPathInUrls(primaryCarePlanMedication, "sierra_leone")
             ]
           }
         };
@@ -819,7 +836,8 @@ angular.module("encounterTypeConfig", ["constants"])
             showOnVisitList: true,
             sections: [
               cloneSectionAndUpdateCountryPathInUrls(delivery, "sierra_leone"),
-              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone")
+              cloneSectionAndUpdateCountryPathInUrls(primaryCareDx, "sierra_leone"),
+              cloneSectionAndUpdateCountryPathInUrls(primaryCarePlanMedication, "sierra_leone")
             ]
           }
         };
