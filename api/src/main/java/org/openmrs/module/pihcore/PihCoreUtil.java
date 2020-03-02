8fbba26bd710e1eafb5f6dfeb8f0036054ef61dd
Brandon Istenes <bistenes@gmail.com>
2020-03-02 14:31:20 -0500

    UHM-4268 Move Drug Lists into openmrs-config-pihemr repos

--- api/src/main/java/org/openmrs/module/pihcore/PihCoreUtil.java
+++ api/src/main/java/org/openmrs/module/pihcore/PihCoreUtil.java
@@ -8,12 +8,24 @@ public class PihCoreUtil {
         return OpenmrsUtil.getApplicationDataDirectory() + "/configuration/pih";
     }
 
+    public static final String getDefaultPihConfigurationChecksumDir() {
+        return OpenmrsUtil.getApplicationDataDirectory() + "/configuration_checksums/pih";
+    }
+
     public static String getFormDirectory() {
         return getDefaultPihConfigurationDir() + "/htmlforms/";
     }
 
     public static String getConceptsMetadataSharingDirectory() {
         return getDefaultPihConfigurationDir() + "/concepts/";
+    }
+
+    public static String getDrugListDirectory() {
+        return getDefaultPihConfigurationDir() + "/drugs/";
+    }
+
+    public static String getDrugListChecksumDirectory() {
+        return PihCoreUtil.getDefaultPihConfigurationChecksumDir() + "/drugs/";
     }
 
     public static String getFormResource(String formName) {
