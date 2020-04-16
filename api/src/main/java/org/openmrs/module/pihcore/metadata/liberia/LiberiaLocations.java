dc4f043b3835f3da58ce2e48ad32f0b7d4c7b5f0
mgoodrich <mark.goodrich@live.com>
2020-04-16 16:35:22 -0400

    UHM-4501: Retire Liberia locations JJ Dossen and Pleebo and migrate data

--- api/src/main/java/org/openmrs/module/pihcore/metadata/liberia/LiberiaLocations.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/liberia/LiberiaLocations.java
@@ -8,42 +8,6 @@ import java.util.List;
 
 public class LiberiaLocations {
 
-    public static LocationDescriptor PLEEBO = new LocationDescriptor() {
-        public String uuid() {
-            return "f6f87d10-f290-11e4-b939-0800200c9a66";
-        }
-
-        public String name() {
-            return "Pleebo";
-        }
-
-        public String description() {
-            return "Pleebo";
-        }
-
-        public List<LocationTagDescriptor> tags() {
-            return Collections.emptyList();
-        }
-    };
-
-    public static LocationDescriptor HARPER = new LocationDescriptor() {
-        public String uuid() {
-            return "6fa7bab5-1ca5-4b77-8c31-80c9589af952";
-        }
-
-        public String name() {
-            return "JJ Dossen";
-        }
-
-        public String description() {
-            return "JJ Dossen";
-        }
-
-        public List<LocationTagDescriptor> tags() {
-            return Collections.emptyList();
-        }
-    };
-
     public static LocationDescriptor HEALTH_FACILITY = new LocationDescriptor() {
         @Override
         public String name() {
@@ -144,4 +108,42 @@ public class LiberiaLocations {
             return LiberiaLocations.HEALTH_FACILITY;
         }
     };
+
+    // legacy, replaced by new "Health Facility" locatin
+    public static LocationDescriptor PLEEBO = new LocationDescriptor() {
+        public String uuid() {
+            return "f6f87d10-f290-11e4-b939-0800200c9a66";
+        }
+
+        public String name() {
+            return "Pleebo";
+        }
+
+        public String description() {
+            return "Pleebo";
+        }
+
+        public List<LocationTagDescriptor> tags() {
+            return Collections.emptyList();
+        }
+    };
+
+    public static LocationDescriptor HARPER = new LocationDescriptor() {
+        public String uuid() {
+            return "6fa7bab5-1ca5-4b77-8c31-80c9589af952";
+        }
+
+        public String name() {
+            return "JJ Dossen";
+        }
+
+        public String description() {
+            return "JJ Dossen";
+        }
+
+        public List<LocationTagDescriptor> tags() {
+            return Collections.emptyList();
+        }
+    };
+
 }
