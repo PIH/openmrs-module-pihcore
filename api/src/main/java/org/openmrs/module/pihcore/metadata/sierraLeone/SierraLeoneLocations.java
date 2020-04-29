87bdd10923d8df8bdfd927dede3620d938b2d960
mgoodrich <mark.goodrich@live.com>
2020-04-29 15:22:50 -0400

    remove comment

--- api/src/main/java/org/openmrs/module/pihcore/metadata/sierraLeone/SierraLeoneLocations.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/sierraLeone/SierraLeoneLocations.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class SierraLeoneLocations {
 
     public static LocationDescriptor WELLBODY_HEALTH_CENTER = new LocationDescriptor() {
-        // TODO we've changed the name and description from Wellbody Health Clinic to Wellbody Clinic, but for some reason it isn't picking up the change
         public String uuid() { return "b6733150-7426-11e5-a837-0800200c9a66"; }
         public String name() { return "Wellbody Clinic"; }
         public String description() { return "Wellbody Clinic"; }
