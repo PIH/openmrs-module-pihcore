a161044d319c7232982c96d0774650f1ed489628
Brandon Istenes <bistenes@gmail.com>
2020-09-14 13:40:07 -0700

    UHM-3122 Removing HIV Program workflow
    https://pihemr.atlassian.net/browse/UHM-3122

--- api/src/main/java/org/openmrs/module/pihcore/metadata/core/program/HIVProgram.java
+++ api/src/main/java/org/openmrs/module/pihcore/metadata/core/program/HIVProgram.java
@@ -13,37 +13,12 @@ import static org.openmrs.module.pihcore.PihCoreConstants.*;
 
 public class HIVProgram {
 
-    public static ProgramWorkflowStateDescriptor HIV_TRANSITION = new ProgramWorkflowStateDescriptor() {
-        public String conceptUuid() { return HIV_PROGRAM_TRANSITION_UUID; }
-        // TODO what exactly do initial and terminal mean again and what should these be?
-        public Boolean initial() { return true; }
-        public Boolean terminal() { return false; }
-        public String uuid() { return "60030941-c0a3-4b84-92e8-2c201fb8102b"; }
-    };
-
-    public static ProgramWorkflowStateDescriptor HIV_ON_ART = new ProgramWorkflowStateDescriptor() {
-        public String conceptUuid() { return HIV_PROGRAM_ON_ART_UUID; }
-        // TODO what exactly  do initial and terminal mean again and what should these be?
-        public Boolean initial() { return false; }
-        public Boolean terminal() { return false; }
-        public String uuid() { return "490a8b6f-4538-4c85-ace9-1535fc634ba7"; }
-    };
-
-    public static ProgramWorkflowDescriptor TREATMENT_STATUS = new ProgramWorkflowDescriptor() {
-        public String conceptUuid() { return HIV_PROGRAM_TREATMENT_STATUS_UUID; }
-        public String uuid() { return "f5022f7d-cdb2-491c-9f06-4f87c9877d17"; }
-        @Override public Set<ProgramWorkflowStateDescriptor> states()
-        { return new HashSet<ProgramWorkflowStateDescriptor>(Arrays.asList(HIV_TRANSITION, HIV_ON_ART)); }
-    };
-
     public static ProgramDescriptor HIV = new ProgramDescriptor() {
         public String conceptUuid() { return HIV_PROGRAM_CONCEPT_UUID; }   // this concept is installed via metadata package
         public String name() { return "HIV"; }
         public String description() { return "HIV Program"; }
         @Override public String outcomesConceptUuid()  { return HIV_PROGRAM_OUTCOMES_CONCEPT_UUID; }   // this concept is installed via metadata package
         public String uuid() { return "b1cb1fc1-5190-4f7a-af08-48870975dafc"; }
-        @Override public Set<ProgramWorkflowDescriptor> workflows() { return Collections.singleton(TREATMENT_STATUS); }
     };
-
 
 }
