4db529831db09f36ecaee0e18c934e1b57c7e37a
mgoodrich <mark.goodrich@live.com>
2020-11-12 17:30:38 -0500

    Add Mental Health Program in Sierra Leone

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneMetadataToInstallAfterConceptsBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/sierraLeone/SierraLeoneMetadataToInstallAfterConceptsBundle.java
@@ -3,6 +3,7 @@ package org.openmrs.module.pihcore.deploy.bundle.sierraLeone;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
 import org.openmrs.module.metadatadeploy.bundle.Requires;
 import org.openmrs.module.pihcore.deploy.bundle.core.program.MCHProgramBundle;
+import org.openmrs.module.pihcore.deploy.bundle.core.program.MentalHealthProgramBundle;
 import org.springframework.stereotype.Component;
 
 // this is a separate bundle that needs to be installed via the Mirebalais Metadata module *after*
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 // TODO: incorporate all PIH metadata installation into this module, remove Mirebalais Metadata module?
 @Component
 @Requires( {
-        MCHProgramBundle.class
+        MCHProgramBundle.class,
+        MentalHealthProgramBundle.class
 } )
 public class SierraLeoneMetadataToInstallAfterConceptsBundle extends AbstractMetadataBundle {
     @Override
