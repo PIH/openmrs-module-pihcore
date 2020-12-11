61603b9c0e87452011ccd6b83b1109bac69cebe8
mgoodrich <mark.goodrich@live.com>
2020-12-11 15:13:09 -0500

    install all program bundles at all sites

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/PihCoreMetadataToInstallAfterConceptsBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/PihCoreMetadataToInstallAfterConceptsBundle.java
@@ -1,4 +1,4 @@
-package org.openmrs.module.pihcore.deploy.bundle.liberia;
+package org.openmrs.module.pihcore.deploy.bundle.core;
 
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
 import org.openmrs.module.metadatadeploy.bundle.Requires;
@@ -8,12 +8,15 @@ import org.springframework.stereotype.Component;
 
 // this is a separate bundle that needs to be installed via the Mirebalais Metadata module *after*
 // concepts have been installed via metadata sharing (since programs reference concepts)
-// TODO: incorporate all PIH metadata installation into this module, remove Mirebalais Metadata module?
 @Component
-@Requires( { PihProgramsBundle.class, OrderEntryConcepts.class  } )
-public class LiberiaMetadataToInstallAfterConceptsBundle extends AbstractMetadataBundle {
+@Requires( {
+        OrderEntryConcepts.class,
+        PihProgramsBundle.class
+} )
+public class PihCoreMetadataToInstallAfterConceptsBundle extends AbstractMetadataBundle {
     @Override
     public void install() throws Exception {
 
     }
 }
+
