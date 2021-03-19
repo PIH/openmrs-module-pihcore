d4b4694c74a37008cd3ee538dbcc7b18075f060d
Brandon Istenes <bistenes@gmail.com>
2021-03-19 15:14:36 -0700

    Update AnalysisRequestsPageController.java

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/peru/AnalysisRequestsPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/peru/AnalysisRequestsPageController.java
@@ -12,16 +12,16 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-// by uiframework convention, this get automatically wired in as the controller for the url "pihcore/peru/analysisRequests.page"
+// by uiframework convention, this gets automatically wired in as the controller for the url "pihcore/peru/analysisRequests.page"
 // (because it's provided by the "pihcore" module and it's in the "peru" subpackage of package "org.openmrs.module.pihcore.page.controller")
-// TODO we could also rename this depending on how it ends up fitting into the whole "analysis requests" workflow
+// TODO we could also rename this depending on how it ends up fitting into the whole workflow
 public class AnalysisRequestsPageController {
 
     public void get(PageModel model,   //  a model we can add data to, that will then passed to the view
                     @SpringBean("encounterService") EncounterService encounterService // wires in the OpenMRS Core Encounter Service API
     )  throws IOException {
 
-        // NOTE:  if you are using the OpenMRS SDK and "watching" this module, you should be able to "hot" reload this page
+        // NOTE: if you are using the OpenMRS SDK and "watching" this module, you should be able to "hot" reload this page
         // and it should automatically pick up changes you've made... you will need to have your IDE build (in IntelliJ I do: Build->Build Project)
         // you also should be able to attach the debugger to this page
 
@@ -37,12 +37,13 @@ public class AnalysisRequestsPageController {
         // calls the API to match all the encounters that match the critera
         List<Encounter> encounters = encounterService.getEncounters(criteria);
 
-        // (see EncounterService class in OpenMRS Core for full API:https://github.com/openmrs/openmrs-core/blob/2.3.x/api/src/main/java/org/openmrs/api/EncounterService.java)
+        // see EncounterService class in OpenMRS Core for full API:
+        //   https://github.com/openmrs/openmrs-core/blob/2.3.x/api/src/main/java/org/openmrs/api/EncounterService.java
 
         // add this list to the model under "encounters"
         model.put("encounters", encounters);
 
-        // we don't need to specific view here... since the controller is named "AnalysisRequestsPageController" and it's in the peru package.
+        // we don't need to specify the view here... since the controller is named "AnalysisRequestsPageController" and it's in the peru package.
         // the view should exist in "webapp/pages/peru/analysisRequests.gsp"
     }
 
