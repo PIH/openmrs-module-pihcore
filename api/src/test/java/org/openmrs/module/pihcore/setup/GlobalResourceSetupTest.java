6762787d56c7b1e59df3b5f9d5a391d08a38b381
Michael Seaton <mseaton@pih.org>
2021-04-01 16:03:09 -0400

    UHM-4169 - Update code the loads in global scripts and styles to ensure file paths used are compatible with the ConfigurationResourceProvider using relative paths (#243)

--- api/src/test/java/org/openmrs/module/pihcore/setup/GlobalResourceSetupTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/setup/GlobalResourceSetupTest.java
@@ -3,15 +3,14 @@ package org.openmrs.module.pihcore.setup;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.pihcore.PihCoreUtil;
+import org.openmrs.ui.framework.resource.ConfigurationResourceProvider;
 import org.openmrs.util.OpenmrsConstants;
 
 import java.io.File;
-import java.util.List;
 import java.util.Properties;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class GlobalResourceSetupTest  {
 
@@ -31,14 +30,18 @@ public class GlobalResourceSetupTest  {
 
     @Test
     public void shouldFindAllGlobalStylesFiles() {
-        List<String> cssFiles = GlobalResourceSetup.getGlobalResourcesFromFileSystemPath(PihCoreUtil.getGlobalStylesDirectory());
-        assertThat(cssFiles, containsInAnyOrder( "configuration/pih/styles/global/global.css", "configuration/pih/styles/global/anotherGlobal.css"));
+        ConfigurationResourceProvider provider = new ConfigurationResourceProvider();
+        assertNotNull(provider.getResource("configuration/pih/styles/global/global.css"));
+        assertNotNull(provider.getResource("configuration/pih/styles/global/anotherGlobal.css"));
+        assertNull(provider.getResource("configuration/pih/styles/global/missingGlobal.css"));
     }
 
     @Test
     public void shouldFindAllGlobalScriptsFiles() {
-        List<String> cssFiles = GlobalResourceSetup.getGlobalResourcesFromFileSystemPath(PihCoreUtil.getGlobalScriptsDirectory());
-        assertThat(cssFiles, containsInAnyOrder("configuration/pih/scripts/global/global.js", "configuration/pih/scripts/global/anotherGlobal.js"));
+        ConfigurationResourceProvider provider = new ConfigurationResourceProvider();
+        assertNotNull(provider.getResource("configuration/pih/scripts/global/global.js"));
+        assertNotNull(provider.getResource("configuration/pih/scripts/global/anotherGlobal.js"));
+        assertNull(provider.getResource("configuration/pih/scripts/global/missingGlobal.js"));
     }
 
 }
