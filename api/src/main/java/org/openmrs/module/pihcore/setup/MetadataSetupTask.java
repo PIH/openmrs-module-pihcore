5ce977c7bd0c079b91ca741d6e3805de5aedffb6
Michael Seaton <mseaton@pih.org>
2021-05-05 09:25:44 -0400

    UHM-5419 - Remove all program constants and bundles from pihcore in order to move these to pihemr-config

--- api/src/main/java/org/openmrs/module/pihcore/setup/MetadataSetupTask.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/MetadataSetupTask.java
@@ -6,9 +6,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.initializer.Domain;
 import org.openmrs.module.metadatadeploy.api.MetadataDeployService;
 import org.openmrs.module.pihcore.config.Config;
-import org.openmrs.module.pihcore.config.ConfigDescriptor;
 import org.openmrs.module.pihcore.deploy.bundle.core.PihCoreMetadataToInstallAfterConceptsBundle;
-import org.openmrs.module.pihcore.deploy.bundle.haiti.HaitiMetadataToInstallAfterConceptsBundle;
 
 public class MetadataSetupTask implements Runnable {
 
@@ -81,10 +79,6 @@ public class MetadataSetupTask implements Runnable {
     private void installMetadataBundlesThatDependOnMDSPackages(Config config) {
         MetadataDeployService deployService = Context.getService(MetadataDeployService.class);
         deployService.installBundle(Context.getRegisteredComponents(PihCoreMetadataToInstallAfterConceptsBundle.class).get(0));
-        // make this more dynamic, less dependent on if-thens
-        if (config.getCountry().equals(ConfigDescriptor.Country.HAITI)) {
-            deployService.installBundle(Context.getRegisteredComponents(HaitiMetadataToInstallAfterConceptsBundle.class).get(0));
-        }
     }
 
 
