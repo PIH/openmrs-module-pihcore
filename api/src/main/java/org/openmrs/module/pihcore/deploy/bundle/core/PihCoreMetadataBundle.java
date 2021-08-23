123419072b6a1a1f5701d498e438bdc3638809cf
Brandon Istenes <bistenes@gmail.com>
2021-08-23 14:15:00 -0700

    Revert "UHM-5559 Migrate Relationship Types to Initializer (#290)"
    This reverts commit f94b496c8ce49ea7fd80c91a41d964ef31e70ea5.
    
    I forgot to update mirebalais

--- api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/PihCoreMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/pihcore/deploy/bundle/core/PihCoreMetadataBundle.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
         ClinicalConsultationConcepts.class,
         SocioEconomicConcepts.class,
         InsuranceConcepts.class,
+        RelationshipTypeBundle.class,
         ProviderRoleBundle.class
         })
 public class PihCoreMetadataBundle extends AbstractMetadataBundle {
