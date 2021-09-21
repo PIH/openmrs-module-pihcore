76dcee8af190cd573646a57239769d884ab93427
Michael Seaton <mseaton@pih.org>
2021-09-20 20:57:31 -0400

    Remove full data export report builder into pihcore and config - step 1.

--- api/src/main/java/org/openmrs/module/pihcore/reporting/library/PihEncounterDataLibrary.java
+++ api/src/main/java/org/openmrs/module/pihcore/reporting/library/PihEncounterDataLibrary.java
@@ -1,9 +1,22 @@
 package org.openmrs.module.pihcore.reporting.library;
 
 import org.openmrs.Concept;
+import org.openmrs.Location;
+import org.openmrs.Obs;
+import org.openmrs.Person;
+import org.openmrs.Provider;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.EncounterService;
 import org.openmrs.module.emrapi.EmrApiProperties;
+import org.openmrs.module.emrapi.disposition.DispositionService;
+import org.openmrs.module.pihcore.PihEmrConfigConstants;
+import org.openmrs.module.reporting.data.converter.ChainedConverter;
+import org.openmrs.module.reporting.data.converter.CollectionConverter;
+import org.openmrs.module.reporting.data.converter.CountConverter;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.converter.ObsValueTextAsCodedConverter;
+import org.openmrs.module.reporting.data.converter.PropertyConverter;
 import org.openmrs.module.reporting.data.encounter.definition.AgeAtEncounterDataDefinition;
 import org.openmrs.module.reporting.data.encounter.definition.AuditInfoEncounterDataDefinition;
 import org.openmrs.module.reporting.data.encounter.definition.ConvertedEncounterDataDefinition;
@@ -17,6 +30,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PihEncounterDataLibrary extends BaseDefinitionLibrary<EncounterDataDefinition> {
+
+    @Autowired
+    ConceptService conceptService;
+
+    @Autowired
+    EncounterService encounterService;
+
+    @Autowired
+    DispositionService dispositionService;
 
     @Autowired
     EmrApiProperties emrApiProperties;
@@ -67,6 +89,109 @@ public class PihEncounterDataLibrary extends BaseDefinitionLibrary<EncounterData
         d.setQuestion(concept);
         d.setSingleObs(true);
         return d;
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getDisposition() {
+        return getSingleObsInEncounter(dispositionService.getDispositionDescriptor().getDispositionConcept());
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getAllProviders() {
+        EncounterProviderDataDefinition dd = new EncounterProviderDataDefinition();
+        dd.setSingleProvider(false);
+        ChainedConverter itemConverter = new ChainedConverter(new PropertyConverter(Provider.class, "person"), new PropertyConverter(Person.class, "personName"),
+                new ObjectFormatter("{givenName} {familyName}"));
+        return new ConvertedEncounterDataDefinition(dd, new CollectionConverter(itemConverter, false, null), new ObjectFormatter(", "));
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getNumberOfProviders() {
+        EncounterProviderDataDefinition dd = new EncounterProviderDataDefinition();
+        dd.setSingleProvider(false);
+        return new ConvertedEncounterDataDefinition(dd, new CountConverter());
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getClerk() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_ADMINISTRATIVECLERK_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getNurse() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_NURSE_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getConsultingClinician() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_CONSULTINGCLINICIAN_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getDispenser() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_DISPENSER_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getRadiologyTechnician() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_RADIOLOGYTECHNICIAN_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getOrderingProvider() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_ORDERINGPROVIDER_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getPrincipalResultsInterpreter() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_PRINCIPALRESULTSINTERPRETER_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getAnesthesiologist() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_ANESTHESIOLOGIST_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getAttendingSurgeonName() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_ATTENDINGSURGEON_UUID);
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getAssistingSurgeon() {
+        return getProvider(PihEmrConfigConstants.ENCOUNTERROLE_ASSISTINGSURGEON_UUID);
+    }
+
+    private EncounterDataDefinition getProvider(String encounterRoleUuid) {
+        EncounterProviderDataDefinition dd = new EncounterProviderDataDefinition();
+        dd.setEncounterRole(encounterService.getEncounterRoleByUuid(encounterRoleUuid));
+        return new ConvertedEncounterDataDefinition(dd,
+                new PropertyConverter(Provider.class, "person"),
+                new PropertyConverter(Person.class, "personName"),
+                new ObjectFormatter("{givenName} {familyName}")
+        );
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getAdmissionStatus() {
+        ObsForEncounterDataDefinition dd = new ObsForEncounterDataDefinition();
+        // concept with synonyms "Type of Patient" and "Admission Status", possible answers are Ambulatory and Hospitalized
+        dd.setQuestion(conceptService.getConceptByMapping("3289", "PIH"));
+        return new ConvertedEncounterDataDefinition(dd, new PropertyConverter(Obs.class, "valueCoded"), new ObjectFormatter());
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getRequestedAdmissionLocationName() {
+        ObsForEncounterDataDefinition dd = new ObsForEncounterDataDefinition();
+        dd.setQuestion(dispositionService.getDispositionDescriptor().getAdmissionLocationConcept());
+        return new ConvertedEncounterDataDefinition(dd, new ObsValueTextAsCodedConverter<Location>(Location.class), new ObjectFormatter());
+    }
+
+    @DocumentedDefinition
+    public EncounterDataDefinition getRequestedTransferLocationName() {
+        ObsForEncounterDataDefinition dd = new ObsForEncounterDataDefinition();
+        dd.setQuestion(dispositionService.getDispositionDescriptor().getInternalTransferLocationConcept());
+        return new ConvertedEncounterDataDefinition(dd, new ObsValueTextAsCodedConverter<Location>(Location.class), new ObjectFormatter());
     }
 
     protected ConvertedEncounterDataDefinition convert(EncounterDataDefinition d, DataConverter... converters) {
