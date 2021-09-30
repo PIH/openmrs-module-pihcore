9c9e69eaf0d5ef6ce9d3027535c5b5ded811581a
Michael Seaton <mseaton@pih.org>
2021-09-29 23:48:00 -0400

    UHM-5824 - Refactor all configuration steps into component that is executed from final activator

--- api/src/main/java/org/openmrs/module/pihcore/setup/PacIntegrationSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/PacIntegrationSetup.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.pihcore.setup;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.pacsintegration.api.PacsIntegrationService;
 import org.openmrs.module.pihcore.config.Components;
@@ -7,10 +9,18 @@ import org.openmrs.module.pihcore.config.Config;
 
 public class PacIntegrationSetup {
 
+    protected static final Log log = LogFactory.getLog(PacIntegrationSetup.class);
+
     public static void setup(Config config) {
 
         if (config.isComponentEnabled(Components.PACS_INTEGRATION)) {
-            Context.getService(PacsIntegrationService.class).initializeHL7Listener();
+            PacsIntegrationService service = Context.getService(PacsIntegrationService.class);
+            if (service.isHL7ListenerRunning()) {
+                log.warn("HL7 Listener is already running, not re-initializing");
+            }
+            else {
+                service.initializeHL7Listener();
+            }
         }
 
     }
