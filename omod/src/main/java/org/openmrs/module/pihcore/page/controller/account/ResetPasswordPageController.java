1f5296aa17275a466ff7d2ccf5131e44f346e82e
Michael Seaton <mseaton@pih.org>
2022-03-02 09:26:28 -0500

    UHM-6250 - Password reset email and page should be localized to current user, even if not authenticated

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/account/ResetPasswordPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/account/ResetPasswordPageController.java
@@ -4,6 +4,8 @@ package org.openmrs.module.pihcore.page.controller.account;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.User;
+import org.openmrs.api.InvalidActivationKeyException;
 import org.openmrs.api.UserService;
 import org.openmrs.api.ValidationException;
 import org.openmrs.api.context.Context;
@@ -15,12 +17,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.openmrs.util.PrivilegeConstants.GET_USERS;
+
 public class ResetPasswordPageController {
 
     protected final Log log = LogFactory.getLog(getClass());
 
-    public void get(@RequestParam(value = "activationKey", required = false) String activationKey, PageModel model) {
-        model.addAttribute("activationKey", activationKey);
+    public String get(@RequestParam(value = "activationKey", required = false) String activationKey,
+                      @SpringBean("userService") UserService userService,
+                      HttpServletRequest request,
+                      PageModel model, UiUtils ui) {
+        try {
+            Context.addProxyPrivilege(GET_USERS);
+            User user = userService.getUserByActivationKey(activationKey);
+            if (user == null) {
+                request.getSession().setAttribute(EmrConstants.SESSION_ATTRIBUTE_ERROR_MESSAGE,
+                        ui.message("activation.key.not.correct", null, Context.getLocale()));
+                return "redirect:index.htm";
+            }
+            Context.setLocale(userService.getDefaultLocaleForUser(user));
+            model.addAttribute("activationKey", activationKey);
+            return null;
+        }
+        finally {
+            Context.removeProxyPrivilege(GET_USERS);
+        }
     }
 
     public String post(@RequestParam(value = "activationKey") String activationKey,
@@ -35,6 +56,12 @@ public class ResetPasswordPageController {
         model.addAttribute("confirmPassword", confirmPassword);
 
         try {
+            Context.addProxyPrivilege(GET_USERS);
+            User user = userService.getUserByActivationKey(activationKey);
+            if (user == null) {
+                throw new InvalidActivationKeyException("activation.key.not.correct");
+            }
+            Context.setLocale(userService.getDefaultLocaleForUser(user));
             if (StringUtils.isBlank(newPassword) || StringUtils.isBlank(confirmPassword)) {
                 throw new ValidationException(ui.message("emr.account.changePassword.newAndConfirmPassword.required"));
             }
@@ -51,6 +78,9 @@ public class ResetPasswordPageController {
                     ui.message("emr.account.changePassword.fail", new Object[]{e.getMessage()}, Context.getLocale())
             );
             log.warn("An error occurred while trying to reset password", e);
+        }
+        finally {
+            Context.removeProxyPrivilege(GET_USERS);
         }
 
         return "redirect:index.htm";
