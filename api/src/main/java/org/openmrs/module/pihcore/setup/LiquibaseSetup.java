46a33e498a879c4d7f2d457a65e9b30af3476d64
mgoodrich <mark.goodrich@live.com>
2022-04-28 14:47:18 -0400

    throw error and stop startup if liquibase update fails

--- api/src/main/java/org/openmrs/module/pihcore/setup/LiquibaseSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/LiquibaseSetup.java
@@ -3,17 +3,19 @@ package org.openmrs.module.pihcore.setup;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.pihcore.PihCoreUtil;
+import org.openmrs.util.DatabaseUpdateException;
 import org.openmrs.util.DatabaseUpdater;
 
 public class LiquibaseSetup {
 
     protected static Log log = LogFactory.getLog(LiquibaseSetup.class);
 
-    public static void setup() {
+    public static void setup() throws Exception {
         try {
             DatabaseUpdater.executeChangelog(PihCoreUtil.getLiquibaseChangeLog(), null);
         } catch (Exception e) {
-            log.error("Unable run liquibase change sets", e);
+            log.error("Unable run liquibase change sets provided by PIH EMR config", e);
+            throw new DatabaseUpdateException(e);
         }
     }
 
