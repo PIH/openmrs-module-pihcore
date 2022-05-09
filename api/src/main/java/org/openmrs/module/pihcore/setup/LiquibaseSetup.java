1016e0d10c592724c07aa20b51caf4c1492567f9
Michael Seaton <mseaton@pih.org>
2022-05-09 12:08:27 -0400

    UHM-6473:  Prevent changesets from re-executing after migrating to OpenMRS 2.5+ (#385)

--- api/src/main/java/org/openmrs/module/pihcore/setup/LiquibaseSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/LiquibaseSetup.java
@@ -2,6 +2,7 @@ package org.openmrs.module.pihcore.setup;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.liquibase.ChangeSetExecutorCallback;
 import org.openmrs.module.pihcore.PihCoreUtil;
 import org.openmrs.util.DatabaseUpdater;
@@ -12,11 +13,23 @@ public class LiquibaseSetup {
 
     public static void setup() throws Exception {
         try {
+            updateLiquibaseChangeLogPath();
             DatabaseUpdater.executeChangelog(PihCoreUtil.getLiquibaseChangeLog(), (ChangeSetExecutorCallback) null);
         } catch (Exception e) {
             log.error("Unable run liquibase change sets provided by PIH EMR config", e);
             throw e;
         }
+    }
+
+    /**
+     * See UHM-6473
+     */
+    private static void updateLiquibaseChangeLogPath() {
+        StringBuilder sql = new StringBuilder();
+        sql.append("update liquibasechangelog ");
+        sql.append("set filename = concat('configuration', substring_index(filename, 'configuration', -1)) ");
+        sql.append("where filename like '%/configuration/pih/liquibase/%' ");
+        Context.getAdministrationService().executeSQL(sql.toString(), false);
     }
 
 }
