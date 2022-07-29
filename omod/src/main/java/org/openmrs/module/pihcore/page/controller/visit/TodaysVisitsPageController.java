0001485f394a357eec1d4d4da98501cd10a8e5f4
louidorjp <jpaullouidor@gmail.com>
2022-07-29 16:31:22 -0400

    HAI-666: display the queue of visits by user's location

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
@@ -5,25 +5,28 @@ import org.openmrs.api.VisitService;
 import org.openmrs.module.emrapi.domainwrapper.DomainWrapperFactory;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.module.appui.UiSessionContext;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Collections;
 
 public class TodaysVisitsPageController {
 
     public void get(PageModel model,
                     @SpringBean("visitService") VisitService visitService,
-                    @SpringBean("domainWrapperFactory")DomainWrapperFactory domainWrapperFactory) {
+                    @SpringBean("domainWrapperFactory")DomainWrapperFactory domainWrapperFactory,
+                    UiSessionContext uiSessionContext) {
 
         LocalDate todayDate = LocalDate.now();
         Date startOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MIN));
         Date endOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MAX));
 
         // all non-voided visits that have been started any time today
-        List<Visit> visits = visitService.getVisits(null, null, null, null, startOfDayToday, endOfDayToday, null, null, null, true, false);
+        List<Visit> visits = visitService.getVisits(null, null, Collections.singletonList(uiSessionContext.getSessionLocation()), null, startOfDayToday, endOfDayToday, null, null, null, true, false);
         model.addAttribute("visits", visits);
     }
 
