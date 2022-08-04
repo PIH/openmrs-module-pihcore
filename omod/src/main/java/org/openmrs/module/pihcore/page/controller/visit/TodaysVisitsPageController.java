ccfe519078947b6b5519a31b57673d9c77b1c2c1
louidorjp <jpaullouidor@gmail.com>
2022-08-03 20:24:31 -0400

    update

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
@@ -6,6 +6,12 @@ import org.openmrs.module.emrapi.domainwrapper.DomainWrapperFactory;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.Location;
+import org.openmrs.LocationTag;
+import org.openmrs.module.emrapi.EmrApiConstants;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.context.Context;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,8 +19,14 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class TodaysVisitsPageController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
 
     public void get(PageModel model,
                     @SpringBean("visitService") VisitService visitService,
@@ -24,9 +36,24 @@ public class TodaysVisitsPageController {
         LocalDate todayDate = LocalDate.now();
         Date startOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MIN));
         Date endOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MAX));
+        
+        Location location=uiSessionContext.getSessionLocation();
+
+        LocationService locationService = Context.getLocationService();
+        LocationTag visitLocationTag =  locationService.getLocationTagByName(EmrApiConstants.LOCATION_TAG_SUPPORTS_VISITS);
+
+        if(location.hasTag(visitLocationTag.toString())){
+            log.error("======================================>"+visitLocationTag.toString());
+
+            location=uiSessionContext.getSessionLocation();
+        }else{
+            log.error("================**********======================>"+location.getParentLocation());
+
+            location=location.getParentLocation();
+        }
 
         //all non-voided visits that have been started any time today at the current session location.
-        List<Visit> visits = visitService.getVisits(null, null, Collections.singletonList(uiSessionContext.getSessionLocation()), null, startOfDayToday, endOfDayToday, null, null, null, true, false);
+        List<Visit> visits = visitService.getVisits(null, null, Collections.singletonList(location), null, startOfDayToday, endOfDayToday, null, null, null, true, false);
         model.addAttribute("visits", visits);
     }
 
