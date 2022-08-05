7f7d512a2cc897f9b09e2214fe31e161a27be3ec
Jean-Paul Louidor <56307966+louidorjp@users.noreply.github.com>
2022-08-05 10:45:30 -0400

    Hai 666 filter the patient queues by site (#396)

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/visit/TodaysVisitsPageController.java
@@ -6,6 +6,11 @@ import org.openmrs.module.emrapi.domainwrapper.DomainWrapperFactory;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.Location;
+import org.openmrs.LocationTag;
+import org.openmrs.module.emrapi.EmrApiConstants;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.context.Context;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,6 +18,8 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Collections;
+
+
 
 public class TodaysVisitsPageController {
 
@@ -24,9 +31,18 @@ public class TodaysVisitsPageController {
         LocalDate todayDate = LocalDate.now();
         Date startOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MIN));
         Date endOfDayToday = java.sql.Timestamp.valueOf(LocalDateTime.of(todayDate, LocalTime.MAX));
+       
+        LocationService locationService = Context.getLocationService();
+        LocationTag visitLocationTag =  locationService.getLocationTagByName(EmrApiConstants.LOCATION_TAG_SUPPORTS_VISITS);
+
+        Location location = uiSessionContext.getSessionLocation();
+
+        while (location != null && !location.hasTag(visitLocationTag.toString())) {
+            location = location.getParentLocation();
+        }
 
         //all non-voided visits that have been started any time today at the current session location.
-        List<Visit> visits = visitService.getVisits(null, null, Collections.singletonList(uiSessionContext.getSessionLocation()), null, startOfDayToday, endOfDayToday, null, null, null, true, false);
+        List<Visit> visits = visitService.getVisits(null, null, location != null ? Collections.singletonList(location) : null, null, startOfDayToday, endOfDayToday, null, null, null, true, false);
         model.addAttribute("visits", visits);
     }
 
