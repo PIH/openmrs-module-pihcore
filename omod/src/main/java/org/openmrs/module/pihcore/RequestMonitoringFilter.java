427535f9dddf3a401d5be0f3849f82f92951c0f3
Michael Seaton <mseaton@pih.org>
2022-10-05 20:35:46 -0400

    UHM-6728 - Activity Log no longer logging after upgrade to 2.5.x

--- omod/src/main/java/org/openmrs/module/pihcore/RequestMonitoringFilter.java
+++ omod/src/main/java/org/openmrs/module/pihcore/RequestMonitoringFilter.java
@@ -3,26 +3,19 @@
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://license.openmrs.org
- *
+ * <p>
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
- *
+ * <p>
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 package org.openmrs.module.pihcore;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Layout;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.RollingFileAppender;
-import org.apache.log4j.helpers.ISO8601DateFormat;
-import org.openmrs.ui.framework.SimpleObject;
-import org.openmrs.util.OpenmrsUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -32,42 +25,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Enumeration;
 
 /**
  * Basic servlet filter to log page requests, utilization, and performance
  */
 public class RequestMonitoringFilter implements Filter {
 
-	private static Logger logger = Logger.getLogger(RequestMonitoringFilter.class);
-
-	private static final String NAME = "RequestMonitoringFileAppender";
-	private static final String LOG_FILE_DIR_NAME = "activitylog";
-	private static final String LOG_FILE_NAME = "activity.log";
-	private static final String ENABLED_PROPERTY = "activitylog_enabled";
-
-	private boolean enabled = false;
+	private static final Logger logger = LogManager.getLogger(RequestMonitoringFilter.class);
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		try {
-			enabled = "true".equalsIgnoreCase(PihCoreUtil.getSystemOrRuntimeProperty(ENABLED_PROPERTY, "false"));
-			logger.setAdditivity(false);
-			File directory = OpenmrsUtil.getDirectoryInApplicationDataDirectory(LOG_FILE_DIR_NAME);
-			directory.mkdirs();
-			File outputFile = new File(directory, LOG_FILE_NAME);
-			Layout layout = new PatternLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN);
-			RollingFileAppender appender = new RollingFileAppender(layout, outputFile.getAbsolutePath(), true);
-			appender.setName(NAME);
-			logger.setLevel(Level.INFO);
-			logger.addAppender(appender);
-		}
-		catch (Exception e) {
-			throw new ServletException("Unable to initialize RequestMonitoringFilter", e);
-		}
 	}
 
 	@Override
@@ -76,53 +44,29 @@ public class RequestMonitoringFilter implements Filter {
 		chain.doFilter(request, response);
 		long endTime = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		if (enabled && request instanceof HttpServletRequest) {
-			try {
-				HttpServletRequest httpRequest = (HttpServletRequest) request;
-				HttpSession session = httpRequest.getSession();
-				ISO8601DateFormat dateFormat = new ISO8601DateFormat();
-				SimpleObject logLine = new SimpleObject();
-				logLine.put("timestamp", dateFormat.format(new Date(startTime)));
-				logLine.put("sessionId", session.getId());
-				logLine.put("user", session.getAttribute("username"));
-				logLine.put("loadTime", totalTime);
-				logLine.put("method", httpRequest.getMethod());
-				logLine.put("requestPath", httpRequest.getRequestURI());
-				logLine.put("queryParams", formatRequestParams(httpRequest));
-				logger.info(logLine.toJson());
-			}
-			catch (Exception e) {
-				// Do nothing, we don't want this filter to cause any adverse behavior
+		if (logger.isTraceEnabled()) {
+			if (request instanceof HttpServletRequest) {
+				try {
+					HttpServletRequest httpRequest = (HttpServletRequest) request;
+					HttpSession session = httpRequest.getSession();
+					ThreadContext.put("sessionId", session.getId());
+					ThreadContext.put("user", (String) session.getAttribute("username"));
+					ThreadContext.put("loadTime", Long.toString(totalTime));
+					ThreadContext.put("method", httpRequest.getMethod());
+					ThreadContext.put("ipAddress", httpRequest.getRemoteAddr());
+					ThreadContext.put("servletPath", httpRequest.getServletPath());
+					ThreadContext.put("requestURI", httpRequest.getRequestURI());
+					logger.trace(httpRequest.getRequestURI() + ": " + totalTime + "ms");
+				} catch (Exception e) {
+					// Do nothing, we don't want this filter to cause any adverse behavior
+				} finally {
+					ThreadContext.clearAll();
+				}
 			}
 		}
 	}
 
 	@Override
 	public void destroy() {
-		logger.removeAppender(NAME);
-	}
-
-	private String formatRequestParams(HttpServletRequest request) {
-		StringBuilder ret = new StringBuilder();
-		if (request.getMethod().equalsIgnoreCase("GET")) {
-			for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements(); ) {
-				String paramName = e.nextElement();
-				String[] paramValues = request.getParameterValues(paramName);
-				if (paramValues != null) {
-					for (String paramValue : paramValues) {
-						if (StringUtils.isNotBlank(paramValue)) {
-							if (ret.length() > 0) {
-								ret.append("&");
-							}
-							if (paramName.toUpperCase().contains("PASSWORD")) {
-								paramValue = "********";
-							}
-							ret.append(paramName).append("=").append(paramValue);
-						}
-					}
-				}
-			}
-		}
-		return ret.toString();
 	}
 }
