676377f3a6252202bb16af0ca60e85fe460ae1d2
Michael Seaton <mseaton@pih.org>
2022-11-04 11:36:25 -0400

    UHM-6804 - Require system admin access to view manage accounts page

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/account/ManageAccountsPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/account/ManageAccountsPageController.java
@@ -1,13 +1,20 @@
 package org.openmrs.module.pihcore.page.controller.account;
 
+import org.openmrs.api.context.Context;
 import org.openmrs.module.emrapi.account.AccountService;
+import org.openmrs.module.pihcore.PihEmrConfigConstants;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 
 public class ManageAccountsPageController {
 
-    public void get(PageModel model, @SpringBean("accountService") AccountService accountService) {
+    public String get(PageModel model,
+                    @SpringBean("accountService") AccountService accountService) {
+        if (!Context.hasPrivilege(PihEmrConfigConstants.PRIVILEGE_APP_EMR_SYSTEM_ADMINISTRATION)) {
+             return "redirect:/index.htm";
+        }
         model.addAttribute("accounts", accountService.getAllAccounts());
+        return "account/manageAccounts";
     }
 
 }
