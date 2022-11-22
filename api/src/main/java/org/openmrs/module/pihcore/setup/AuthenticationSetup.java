af385f6ac2340ad09309d5047af5d470378f12e0
Michael Seaton <mseaton@pih.org>
2022-11-22 10:30:29 -0500

    UHM-6876 - Extract login pages to shared module

--- api/src/main/java/org/openmrs/module/pihcore/setup/AuthenticationSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/AuthenticationSetup.java
@@ -63,11 +63,16 @@ public class AuthenticationSetup {
 
         // Basic Authentication Scheme.  This provides basic auth + session location selection
         {
-            String className = "org.openmrs.module.pihcore.PihBasicAuthenticationScheme";
+            String className = "org.openmrs.module.authentication.web.BasicWithLocationAuthenticationScheme";
             Properties p = new Properties();
             p.put("loginPage", "/authenticationui/login/login.page");
             p.put("usernameParam", "username");
             p.put("passwordParam", "password");
+            p.put("locationParamName", "sessionLocation");
+            p.put("locationRequired", "true");
+            p.put("onlyLocationsWithTag", "Login Location");
+            p.put("locationSessionAttributeName", "emrContext.sessionLocationId");
+            p.put("lastLocationCookieName", "emr.lastSessionLocation");
             addScheme(BASIC, className, p, whitelist);
         }
 
