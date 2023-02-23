10c8c7c84b67599748e6bce1418b02cda7353ca7
Michael Seaton <mseaton@pih.org>
2023-02-23 14:21:52 -0500

    Add runtime/system property to explicitly disable the dbevent setup if desired. (#423)

--- api/src/main/java/org/openmrs/module/pihcore/setup/DbEventSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/DbEventSetup.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.pihcore.setup;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.dbevent.DbEventSource;
@@ -8,18 +9,33 @@ import org.openmrs.module.dbevent.EventContext;
 import org.openmrs.module.pihcore.config.Components;
 import org.openmrs.module.pihcore.config.Config;
 import org.openmrs.module.pihcore.event.PatientUpdateEventConsumer;
+import org.openmrs.util.ConfigUtil;
 
 import java.util.Set;
+
+import static org.openmrs.module.pihcore.PihCoreConstants.DBEVENT_ENABLED_PROPERTY;
 
 public class DbEventSetup {
 
     protected static Log log = LogFactory.getLog(DbEventSetup.class);
 
     /**
+     * @param config the pih config
+     * @return true if the DB Event handling is enabled on this instance
+     */
+    public static boolean isEnabled(Config config) {
+        if (config.isComponentEnabled(Components.DB_EVENT)) {
+            String enabled = ConfigUtil.getProperty(DBEVENT_ENABLED_PROPERTY, "true");
+            return BooleanUtils.toBoolean(enabled);
+        }
+        return false;
+    }
+
+    /**
      * Setup debezium event sources and consumers
      */
     public static void setup(Config config) {
-        if (config.isComponentEnabled(Components.DB_EVENT)) {
+        if (isEnabled(config)) {
             EventContext context = new EventContext();
             DbEventSource eventSource = getEventSource(context);
             eventSource.start();
