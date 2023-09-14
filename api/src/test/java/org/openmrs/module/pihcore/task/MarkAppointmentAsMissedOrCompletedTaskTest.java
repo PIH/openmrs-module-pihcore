4b3328159bd745289d47779ab339b1931643e3ce
Michael Seaton <mseaton@pih.org>
2023-09-14 17:48:13 -0400

    UHM-7376: Rename Appointment to PatientAppointment

--- api/src/test/java/org/openmrs/module/pihcore/task/MarkAppointmentAsMissedOrCompletedTaskTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/task/MarkAppointmentAsMissedOrCompletedTaskTest.java
@@ -7,7 +7,7 @@ import org.openmrs.Location;
 import org.openmrs.Visit;
 import org.openmrs.api.LocationService;
 import org.openmrs.contrib.testdata.TestDataManager;
-import org.openmrs.module.appointmentscheduling.Appointment;
+import org.openmrs.module.appointmentscheduling.PatientAppointment;
 import org.openmrs.module.appointmentscheduling.api.AppointmentService;
 import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.module.emrapi.EmrApiProperties;
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.openmrs.module.appointmentscheduling.Appointment.AppointmentStatus;
+import static org.openmrs.module.appointmentscheduling.PatientAppointment.AppointmentStatus;
 
 public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSensitiveTest {
 
@@ -59,16 +59,16 @@ public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSe
 
         new MarkAppointmentsAsMissedOrCompletedTask().run();
 
-        assertThat(appointmentService.getAppointment(1).getStatus(), is(AppointmentStatus.MISSED));
-        assertThat(appointmentService.getAppointment(2).getStatus(), is(AppointmentStatus.MISSED));
+        assertThat(appointmentService.getPatientAppointment(1).getStatus(), is(AppointmentStatus.MISSED));
+        assertThat(appointmentService.getPatientAppointment(2).getStatus(), is(AppointmentStatus.MISSED));
 
         // status of other appointments should not be changed
-        assertThat(appointmentService.getAppointment(3).getStatus(), is(AppointmentStatus.COMPLETED));
-        assertThat(appointmentService.getAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
-        assertThat(appointmentService.getAppointment(5).getStatus(), is(AppointmentStatus.CANCELLED));
-        assertThat(appointmentService.getAppointment(6).getStatus(), is(AppointmentStatus.CANCELLED_AND_NEEDS_RESCHEDULE));
-        assertThat(appointmentService.getAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
-        assertThat(appointmentService.getAppointment(8).getStatus(), is(AppointmentStatus.WAITING));
+        assertThat(appointmentService.getPatientAppointment(3).getStatus(), is(AppointmentStatus.COMPLETED));
+        assertThat(appointmentService.getPatientAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
+        assertThat(appointmentService.getPatientAppointment(5).getStatus(), is(AppointmentStatus.CANCELLED));
+        assertThat(appointmentService.getPatientAppointment(6).getStatus(), is(AppointmentStatus.CANCELLED_AND_NEEDS_RESCHEDULE));
+        assertThat(appointmentService.getPatientAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
+        assertThat(appointmentService.getPatientAppointment(8).getStatus(), is(AppointmentStatus.WAITING));
 
     }
 
@@ -77,8 +77,8 @@ public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSe
 
         new MarkAppointmentsAsMissedOrCompletedTask().run();
 
-        assertThat(appointmentService.getAppointment(9).getStatus(), is(AppointmentStatus.SCHEDULED));
-        assertThat(appointmentService.getAppointment(10).getStatus(), is(AppointmentStatus.WAITING));
+        assertThat(appointmentService.getPatientAppointment(9).getStatus(), is(AppointmentStatus.SCHEDULED));
+        assertThat(appointmentService.getPatientAppointment(10).getStatus(), is(AppointmentStatus.WAITING));
 
     }
 
@@ -88,24 +88,24 @@ public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSe
         new MarkAppointmentsAsMissedOrCompletedTask().run();
 
         // sanity check: status isn't changed by default, since there are no consultations associated with this visit
-        assertThat(appointmentService.getAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
-        assertThat(appointmentService.getAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
-        assertThat(appointmentService.getAppointment(8).getStatus(), is(AppointmentStatus.WAITING));
+        assertThat(appointmentService.getPatientAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
+        assertThat(appointmentService.getPatientAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
+        assertThat(appointmentService.getPatientAppointment(8).getStatus(), is(AppointmentStatus.WAITING));
 
         Location location1 = locationService.getLocation(1);
         Location location3 = locationService.getLocation(3);  // location of the appointments, as defined in test dataset
 
         // now add a visit to one appointment, and a visit with a consult to the other
-        Appointment appt4 = appointmentService.getAppointment(4);
+        PatientAppointment appt4 = appointmentService.getPatientAppointment(4);
         Visit visit4 = testDataManager.visit()
                 .patient(appt4.getPatient())
                 .visitType(1)
                 .started(new DateTime(2005, 1, 1, 0, 0, 0).toDate())
                 .save();
         appt4.setVisit(visit4);
-        appointmentService.saveAppointment(appt4);
+        appointmentService.savePatientAppointment(appt4);
 
-        Appointment appt7 = appointmentService.getAppointment(7);
+        PatientAppointment appt7 = appointmentService.getPatientAppointment(7);
         Visit visit7 = testDataManager.visit()
                 .patient(appt7.getPatient())
                 .visitType(1)
@@ -118,9 +118,9 @@ public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSe
                         .save())
                 .save();
         appt7.setVisit(visit7);
-        appointmentService.saveAppointment(appt7);
+        appointmentService.savePatientAppointment(appt7);
 
-        Appointment appt8 = appointmentService.getAppointment(8);
+        PatientAppointment appt8 = appointmentService.getPatientAppointment(8);
         Visit visit8 = testDataManager.visit()
                 .patient(appt8.getPatient())
                 .visitType(1)
@@ -133,19 +133,19 @@ public class MarkAppointmentAsMissedOrCompletedTaskTest extends PihCoreContextSe
                         .save())
                 .save();
         appt8.setVisit(visit8);
-        appointmentService.saveAppointment(appt8);
+        appointmentService.savePatientAppointment(appt8);
 
         // run the task again
         new MarkAppointmentsAsMissedOrCompletedTask().run();
 
         // should not be changed because associated visit did not have consult
-        assertThat(appointmentService.getAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
+        assertThat(appointmentService.getPatientAppointment(4).getStatus(), is(AppointmentStatus.INCONSULTATION));
 
         // should not be changed since associated visit did not have consult at location
-        assertThat(appointmentService.getAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
+        assertThat(appointmentService.getPatientAppointment(7).getStatus(), is(AppointmentStatus.WALKIN));
 
         // should be changed to COMPLETED since associated visit had consult at location
-        assertThat(appointmentService.getAppointment(8).getStatus(), is(AppointmentStatus.COMPLETED));
+        assertThat(appointmentService.getPatientAppointment(8).getStatus(), is(AppointmentStatus.COMPLETED));
 
     }
 
