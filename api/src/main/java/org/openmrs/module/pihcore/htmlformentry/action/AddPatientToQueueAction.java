622327709f219200cf2cb64f15f5bafdcc970cdf
Cosmin <cioan@pih.org>
2024-02-06 11:14:36 -0500

    SL-480: Add patient to queue via post submission action (#473)
    * SL-480: Add patient to queue via post submission action

---
+++ api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/AddPatientToQueueAction.java
@@ -0,0 +1,103 @@
+package org.openmrs.module.pihcore.htmlformentry.action;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.htmlformentry.CustomFormSubmissionAction;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.queue.api.QueueServicesWrapper;
+import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
+import org.openmrs.module.queue.api.search.QueueSearchCriteria;
+import org.openmrs.module.queue.exception.DuplicateQueueEntryException;
+import org.openmrs.module.queue.model.Queue;
+import org.openmrs.module.queue.model.QueueEntry;
+import org.openmrs.module.queue.utils.QueueUtils;
+
+/**
+ * Custom action that adds a patient to a queue, if the encounter contains a top level obs with the
+ * following structure
+ * <obs conceptId="CIEL:1272"  answerConceptId="PIH:14976"  />
+ * where answerConceptId points to the service concept associated with a queue.
+ * It assumes that the service concept maps to one and only one queue.
+ */
+public class AddPatientToQueueAction implements CustomFormSubmissionAction {
+
+    private static final String ADD_TO_QUEUE_CONCEPT_CODE_PIH = "1272";
+    protected Log log = LogFactory.getLog(getClass());
+
+    @Override
+    public void applyAction(FormEntrySession formEntrySession) {
+
+        Concept addToQueueConcept = Context.getConceptService().getConceptByMapping(ADD_TO_QUEUE_CONCEPT_CODE_PIH, "PIH");
+
+        if (addToQueueConcept == null) {
+            throw new RuntimeException("AddPatientToQueueAction not executing as Concept PIH: " + ADD_TO_QUEUE_CONCEPT_CODE_PIH + " cannot be found");
+        }
+        Patient patient = formEntrySession.getPatient();
+        Encounter encounter = formEntrySession.getEncounter();
+
+        for (Obs candidate : encounter.getObsAtTopLevel(false)) {
+            if (candidate.getConcept().equals(addToQueueConcept)) {
+                // The coded answer represents the Concept associated with the queue service
+                Concept serviceQueue = candidate.getValueCoded();
+                QueueSearchCriteria criteria = new QueueSearchCriteria();
+                criteria.setServices(Arrays.asList(serviceQueue));
+                QueueServicesWrapper queueServicesWrapper = Context.getRegisteredComponents(QueueServicesWrapper.class).get(0);
+                List<Queue> queues = queueServicesWrapper.getQueueService().getQueues(criteria);
+                Queue queue = null ;
+                QueueEntry queueEntry = null;
+                if ( queues.isEmpty() ) {
+                    throw new RuntimeException("AddPatientToQueueAction not executing as no queue with service concept " + serviceQueue.getName() + " could be found ");
+                }
+                if ( queues.size() > 1 ) {
+                    throw new RuntimeException("AddPatientToQueueAction not executing as there are more than one queue with service concept " + serviceQueue.getName());
+                }
+                queue = queues.get(0);
+
+                queueEntry = new QueueEntry();
+                queueEntry.setQueue(queue);
+                queueEntry.setPatient(patient);
+
+                List<Concept> allowedStatuses = queueServicesWrapper.getAllowedStatuses(queue);
+                if (!allowedStatuses.isEmpty()) {
+                    queueEntry.setStatus(allowedStatuses.get(0));
+                }
+
+                List<Concept> allowedPriorities = queueServicesWrapper.getAllowedPriorities(queue);
+                if (!allowedPriorities.isEmpty()) {
+                    queueEntry.setPriority(allowedPriorities.get(0));
+                }
+                queueEntry.setStartedAt(encounter.getEncounterDatetime());
+
+                QueueEntrySearchCriteria searchCriteria = new QueueEntrySearchCriteria();
+                searchCriteria.setPatient(patient);
+                searchCriteria.setQueues(Collections.singletonList(queue));
+                List<QueueEntry> queueEntries = queueServicesWrapper.getQueueEntryService().getQueueEntries(searchCriteria);
+                boolean isPatientAlreadyOnTheQueue = false;
+                for (QueueEntry entry : queueEntries) {
+                    if (QueueUtils.datesOverlap(entry.getStartedAt(), entry.getEndedAt(), queueEntry.getStartedAt(), queueEntry.getEndedAt())) {
+                        isPatientAlreadyOnTheQueue = true;
+                        break;
+                    }
+
+                }
+                if (!isPatientAlreadyOnTheQueue) {
+                    try {
+                        queueServicesWrapper.getQueueEntryService().saveQueueEntry(queueEntry);
+                    } catch (Exception e) {
+                        throw new RuntimeException("AddPatientToQueueAction failed to add patient to queue", e);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ api/src/test/java/org/openmrs/module/pihcore/action/AddPatientToQueueActionTest.java
@@ -0,0 +1,179 @@
+package org.openmrs.module.pihcore.action;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Location;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.module.queue.api.QueueEntryService;
+import org.openmrs.module.queue.api.dao.QueueDao;
+import org.openmrs.module.queue.api.dao.QueueEntryDao;
+import org.openmrs.module.queue.api.impl.QueueEntryServiceImpl;
+import org.openmrs.module.queue.api.impl.QueueServiceImpl;
+import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
+import org.openmrs.module.queue.model.Queue;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.htmlformentry.FormEntryContext;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.FormSubmissionActions;
+import org.openmrs.module.pihcore.htmlformentry.action.AddPatientToQueueAction;
+import org.openmrs.module.queue.api.QueueService;
+import org.openmrs.module.queue.api.QueueServicesWrapper;
+import org.openmrs.module.queue.api.search.QueueSearchCriteria;
+import org.openmrs.module.queue.model.QueueEntry;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Context.class)
+@PowerMockIgnore("javax.management.*")
+public class AddPatientToQueueActionTest {
+
+    private static final String ADD_TO_QUEUE_CONCEPT_CODE_PIH = "1272";
+    private FormEntrySession mockFormEntrySession;
+    private FormEntryContext mockFormEntryContext;
+    private FormSubmissionActions mockFormSubmissionActions;
+    private AddPatientToQueueAction addPatientToQueueAction;
+
+    private ConceptService conceptService;
+    private Concept addToQueueConcept;
+    private Concept mcoeTriageService;
+    private QueueServicesWrapper wrapper;
+    private QueueService queueServiceImpl;
+    private QueueEntryService queueEntryServiceImpl;
+    private QueueDao mockDao;
+    private QueueEntryDao mockQueueEntryDao;
+
+    @Captor
+    ArgumentCaptor<QueueEntry> queueEntryArgumentCaptor;
+
+    @Before
+    public void setUp()  {
+
+        mockFormEntrySession = mock(FormEntrySession.class);
+        mockFormEntryContext = mock(FormEntryContext.class);
+        mockFormSubmissionActions = mock(FormSubmissionActions.class);
+
+        addPatientToQueueAction = new AddPatientToQueueAction();
+        when(mockFormEntrySession.getSubmissionActions()).thenReturn(mockFormSubmissionActions);
+        when(mockFormEntrySession.getContext()).thenReturn(mockFormEntryContext);
+        conceptService = mock(ConceptService.class);
+        addToQueueConcept = new Concept();
+        mcoeTriageService = new Concept();
+        mockStatic(Context.class);
+        when(Context.getConceptService()).thenReturn(conceptService);
+        when(conceptService.getConceptByMapping(ADD_TO_QUEUE_CONCEPT_CODE_PIH, "PIH")).thenReturn(addToQueueConcept);
+        wrapper = mock(QueueServicesWrapper.class);
+
+        when(Context.getRegisteredComponents(QueueServicesWrapper.class)).thenReturn(Arrays.asList(wrapper));
+        mockDao = mock(QueueDao.class);
+        queueServiceImpl = new QueueServiceImpl();
+        ((QueueServiceImpl) queueServiceImpl).setDao(mockDao);
+        when(wrapper.getQueueService()).thenReturn(queueServiceImpl);
+
+        mockQueueEntryDao = mock(QueueEntryDao.class);
+        queueEntryServiceImpl = new QueueEntryServiceImpl();
+        ((QueueEntryServiceImpl) queueEntryServiceImpl).setDao(mockQueueEntryDao);
+        when(wrapper.getQueueEntryService()).thenReturn(queueEntryServiceImpl);
+    }
+
+    @Test
+    public void applyAction_shouldAddPatientToQueue() {
+        Patient patient = new Patient();
+        Encounter encounter = new Encounter();
+        encounter.setPatient(patient);
+        encounter.setEncounterDatetime(new Date());
+        Location location = new Location();
+        encounter.setLocation(location);
+        Obs referToMCOE = new Obs(patient, addToQueueConcept, encounter.getEncounterDatetime(), location);
+        referToMCOE.setValueCoded(mcoeTriageService);
+        encounter.addObs(referToMCOE);
+        Visit visit = new Visit();
+        visit.addEncounter(encounter);
+        Queue queue = new Queue();
+        queue.setService(mcoeTriageService);
+        QueueSearchCriteria searchCriteria = new QueueSearchCriteria();
+        searchCriteria.setServices(Arrays.asList(mcoeTriageService));
+        QueueEntry queueEntry = new QueueEntry();
+        queueEntry.setPatient(patient);
+        queueEntry.setQueue(queue);
+        queueEntry.setStartedAt(encounter.getEncounterDatetime());
+
+        when(mockFormEntrySession.getPatient()).thenReturn(patient);
+        when(mockFormEntrySession.getEncounter()).thenReturn(encounter);
+        when(mockFormEntryContext.getVisit()).thenReturn(visit);
+        when(mockDao.getQueues(searchCriteria)).thenReturn(Arrays.asList(queue));
+        when(mockQueueEntryDao.createOrUpdate(queueEntry)).thenReturn(queueEntry);
+
+        addPatientToQueueAction.applyAction(mockFormEntrySession);
+        // verify that a new QueueEntry is added to the queue
+        verify(mockQueueEntryDao).createOrUpdate(any());
+        verify(mockQueueEntryDao).createOrUpdate(queueEntryArgumentCaptor.capture());
+        QueueEntry capturedValue = queueEntryArgumentCaptor.getValue();
+        assertThat(capturedValue.getQueue(), is(queue));
+        assertThat(capturedValue.getPatient(), is(patient));
+        assertThat(capturedValue.getStartedAt(), is(encounter.getEncounterDatetime()));
+    }
+
+    @Test
+    public void applyAction_shouldNotAddPatientToQueueIfPatientIsAlreadyOnTheQueue() {
+        Patient patient = new Patient();
+        Encounter encounter = new Encounter();
+        encounter.setPatient(patient);
+        encounter.setEncounterDatetime(new Date());
+        Location location = new Location();
+        encounter.setLocation(location);
+        Obs referToMCOE = new Obs(patient, addToQueueConcept, encounter.getEncounterDatetime(), location);
+        referToMCOE.setValueCoded(mcoeTriageService);
+        encounter.addObs(referToMCOE);
+        Visit visit = new Visit();
+        visit.addEncounter(encounter);
+        Queue queue = new Queue();
+        queue.setService(mcoeTriageService);
+        QueueSearchCriteria searchCriteria = new QueueSearchCriteria();
+        searchCriteria.setServices(Arrays.asList(mcoeTriageService));
+        QueueEntry queueEntry = new QueueEntry();
+        queueEntry.setPatient(patient);
+        queueEntry.setQueue(queue);
+        queueEntry.setStartedAt(encounter.getEncounterDatetime());
+        queue.addQueueEntry(queueEntry);
+
+        when(mockFormEntrySession.getPatient()).thenReturn(patient);
+        when(mockFormEntrySession.getEncounter()).thenReturn(encounter);
+        when(mockFormEntryContext.getVisit()).thenReturn(visit);
+        when(mockDao.getQueues(searchCriteria)).thenReturn(Arrays.asList(queue));
+        when(mockQueueEntryDao.createOrUpdate(queueEntry)).thenReturn(queueEntry);
+
+        QueueEntrySearchCriteria qeCriteria = new QueueEntrySearchCriteria();
+        qeCriteria.setPatient(patient);
+        qeCriteria.setQueues(Collections.singletonList(queue));
+        when(mockQueueEntryDao.getQueueEntries(qeCriteria)).thenReturn(Arrays.asList(queueEntry));
+
+        addPatientToQueueAction.applyAction(mockFormEntrySession);
+        // a new QueueEntry is never added/saved to the db
+        verify(mockQueueEntryDao, never()).createOrUpdate(any());
+
+    }
+}
