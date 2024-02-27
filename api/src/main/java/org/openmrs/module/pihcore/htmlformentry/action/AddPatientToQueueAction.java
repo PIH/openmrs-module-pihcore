2e96456a86445166662e3ca761719c62b823d8ab
Cosmin <cioan@pih.org>
2024-02-26 19:44:44 -0500

    UHM-7716: prevent adding queue entries for past visits (#481)
    * UHM-7716: prevent adding queue entries for past visits

--- api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/AddPatientToQueueAction.java
+++ api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/AddPatientToQueueAction.java
@@ -2,7 +2,6 @@ package org.openmrs.module.pihcore.htmlformentry.action;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import org.openmrs.Concept;
 import org.openmrs.Encounter;
@@ -10,6 +9,7 @@ import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.CustomFormSubmissionAction;
+import org.openmrs.module.htmlformentry.FormEntryContext;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 
 import org.apache.commons.logging.Log;
@@ -17,7 +17,6 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.queue.api.QueueServicesWrapper;
 import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
 import org.openmrs.module.queue.api.search.QueueSearchCriteria;
-import org.openmrs.module.queue.exception.DuplicateQueueEntryException;
 import org.openmrs.module.queue.model.Queue;
 import org.openmrs.module.queue.model.QueueEntry;
 import org.openmrs.module.queue.utils.QueueUtils;
@@ -37,66 +36,66 @@ public class AddPatientToQueueAction implements CustomFormSubmissionAction {
     @Override
     public void applyAction(FormEntrySession formEntrySession) {
 
-        Concept addToQueueConcept = Context.getConceptService().getConceptByMapping(ADD_TO_QUEUE_CONCEPT_CODE_PIH, "PIH");
+        if (formEntrySession.getContext().getMode().equals(FormEntryContext.Mode.ENTER)) {
+            // we execute this post submission action only in the ENTER mode
+            Concept addToQueueConcept = Context.getConceptService().getConceptByMapping(ADD_TO_QUEUE_CONCEPT_CODE_PIH, "PIH");
 
-        if (addToQueueConcept == null) {
-            throw new RuntimeException("AddPatientToQueueAction not executing as Concept PIH: " + ADD_TO_QUEUE_CONCEPT_CODE_PIH + " cannot be found");
-        }
-        Patient patient = formEntrySession.getPatient();
-        Encounter encounter = formEntrySession.getEncounter();
+            if (addToQueueConcept == null) {
+                throw new RuntimeException("AddPatientToQueueAction not executing as Concept PIH: " + ADD_TO_QUEUE_CONCEPT_CODE_PIH + " cannot be found");
+            }
+            Patient patient = formEntrySession.getPatient();
+            Encounter encounter = formEntrySession.getEncounter();
 
-        for (Obs candidate : encounter.getObsAtTopLevel(false)) {
-            if (candidate.getConcept().equals(addToQueueConcept)) {
-                // The coded answer represents the Concept associated with the queue service
-                Concept serviceQueue = candidate.getValueCoded();
-                QueueSearchCriteria criteria = new QueueSearchCriteria();
-                criteria.setServices(Arrays.asList(serviceQueue));
-                QueueServicesWrapper queueServicesWrapper = Context.getRegisteredComponents(QueueServicesWrapper.class).get(0);
-                List<Queue> queues = queueServicesWrapper.getQueueService().getQueues(criteria);
-                Queue queue = null ;
-                QueueEntry queueEntry = null;
-                if ( queues.isEmpty() ) {
-                    throw new RuntimeException("AddPatientToQueueAction not executing as no queue with service concept " + serviceQueue.getName() + " could be found ");
-                }
-                if ( queues.size() > 1 ) {
-                    throw new RuntimeException("AddPatientToQueueAction not executing as there are more than one queue with service concept " + serviceQueue.getName());
-                }
-                queue = queues.get(0);
-
-                queueEntry = new QueueEntry();
-                queueEntry.setQueue(queue);
-                queueEntry.setPatient(patient);
-
-                List<Concept> allowedStatuses = queueServicesWrapper.getAllowedStatuses(queue);
-                if (!allowedStatuses.isEmpty()) {
-                    queueEntry.setStatus(allowedStatuses.get(0));
-                }
-
-                List<Concept> allowedPriorities = queueServicesWrapper.getAllowedPriorities(queue);
-                if (!allowedPriorities.isEmpty()) {
-                    queueEntry.setPriority(allowedPriorities.get(0));
-                }
-                queueEntry.setStartedAt(encounter.getEncounterDatetime());
-                queueEntry.setVisit(encounter.getVisit());
-
-                QueueEntrySearchCriteria searchCriteria = new QueueEntrySearchCriteria();
-                searchCriteria.setPatient(patient);
-                searchCriteria.setQueues(Collections.singletonList(queue));
-                List<QueueEntry> queueEntries = queueServicesWrapper.getQueueEntryService().getQueueEntries(searchCriteria);
-                boolean isPatientAlreadyOnTheQueue = false;
-                for (QueueEntry entry : queueEntries) {
-                    if (QueueUtils.datesOverlap(entry.getStartedAt(), entry.getEndedAt(), queueEntry.getStartedAt(), queueEntry.getEndedAt())) {
-                        isPatientAlreadyOnTheQueue = true;
-                        break;
+            for (Obs candidate : encounter.getObsAtTopLevel(false)) {
+                if (candidate.getConcept().equals(addToQueueConcept)) {
+                    // The coded answer represents the Concept associated with the queue service
+                    Concept serviceQueue = candidate.getValueCoded();
+                    QueueSearchCriteria criteria = new QueueSearchCriteria();
+                    criteria.setServices(Arrays.asList(serviceQueue));
+                    QueueServicesWrapper queueServicesWrapper = Context.getRegisteredComponents(QueueServicesWrapper.class).get(0);
+                    List<Queue> queues = queueServicesWrapper.getQueueService().getQueues(criteria);
+                    if (queues.isEmpty()) {
+                        throw new RuntimeException("AddPatientToQueueAction not executing as no queue with service concept " + serviceQueue.getName() + " could be found ");
                     }
-
-                }
-                if (!isPatientAlreadyOnTheQueue) {
-                    try {
-                        queueServicesWrapper.getQueueEntryService().saveQueueEntry(queueEntry);
-                    } catch (Exception e) {
-                        throw new RuntimeException("AddPatientToQueueAction failed to add patient to queue", e);
+                    if (queues.size() > 1) {
+                        throw new RuntimeException("AddPatientToQueueAction not executing as there are more than one queue with service concept " + serviceQueue.getName());
                     }
+                    Queue queue = queues.get(0);
+
+                    QueueEntrySearchCriteria searchCriteria = new QueueEntrySearchCriteria();
+                    searchCriteria.setPatient(patient);
+                    searchCriteria.setQueues(Collections.singletonList(queue));
+                    searchCriteria.setIsEnded(null);
+                    List<QueueEntry> queueEntries = queueServicesWrapper.getQueueEntryService().getQueueEntries(searchCriteria);
+                    boolean isPatientAlreadyOnTheQueue = false;
+                    for (QueueEntry entry : queueEntries) {
+                        if (QueueUtils.datesOverlap(entry.getStartedAt(), entry.getEndedAt(), encounter.getEncounterDatetime(), null)) {
+                            isPatientAlreadyOnTheQueue = true;
+                            break;
+                        }
+                    }
+                    if (!isPatientAlreadyOnTheQueue) {
+                        try {
+                            QueueEntry queueEntry = new QueueEntry();
+                            queueEntry.setQueue(queue);
+                            queueEntry.setPatient(patient);
+                            List<Concept> allowedStatuses = queueServicesWrapper.getAllowedStatuses(queue);
+                            if (!allowedStatuses.isEmpty()) {
+                                queueEntry.setStatus(allowedStatuses.get(0));
+                            }
+                            List<Concept> allowedPriorities = queueServicesWrapper.getAllowedPriorities(queue);
+                            if (!allowedPriorities.isEmpty()) {
+                                queueEntry.setPriority(allowedPriorities.get(0));
+                            }
+                            queueEntry.setStartedAt(encounter.getEncounterDatetime());
+                            queueEntry.setVisit(encounter.getVisit());
+                            queueServicesWrapper.getQueueEntryService().saveQueueEntry(queueEntry);
+                        } catch (Exception e) {
+                            throw new RuntimeException("AddPatientToQueueAction failed to add patient to queue", e);
+                        }
+                    }
+                    // no need to continue iterating through the encounter obs, it's only one obs that triggers the post-submit action
+                    break;
                 }
             }
         }
