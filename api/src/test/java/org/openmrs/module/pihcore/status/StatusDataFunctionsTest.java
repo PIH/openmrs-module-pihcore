4db4a0bbaf7e469b5b75bc11f8857ef2c0400694
Mark Goodrich <mark.goodrich@live.com>
2024-02-29 14:34:36 -0500

    UHM-7774: VelocityTests failing (#484)

--- api/src/test/java/org/openmrs/module/pihcore/status/StatusDataFunctionsTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/status/StatusDataFunctionsTest.java
@@ -19,20 +19,30 @@ public class StatusDataFunctionsTest {
         Calendar cal = Calendar.getInstance();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         String today = df.format(cal.getTime());
+
+        cal = Calendar.getInstance();
         cal.add(Calendar.YEAR, 3);
         dataValues.put("in3Years", cal.getTime());
-        cal.add(Calendar.YEAR, -3);
+
+        cal = Calendar.getInstance();
         cal.add(Calendar.DATE, 52);
         dataValues.put("in52Days", cal.getTime());
-        cal.add(Calendar.DATE, -52);
+
+        cal = Calendar.getInstance();
         cal.add(Calendar.DATE, -1);
         dataValues.put("yesterday", cal.getTime());
         String yesterday = df.format(cal.getTime());
-        cal.add(Calendar.DATE, -7);
+
+        cal = Calendar.getInstance();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
         dataValues.put("lastWeek", cal.getTime());
         String lastWeek = df.format(cal.getTime());
+
+        cal = Calendar.getInstance();
         cal.add(Calendar.MONTH, -1);
         dataValues.put("lastMonth", cal.getTime());
+
+        cal = Calendar.getInstance();
         cal.add(Calendar.YEAR, -1);
         dataValues.put("lastYear", cal.getTime());
 
@@ -41,7 +51,7 @@ public class StatusDataFunctionsTest {
         testExpression(dataValues, today, "$fn.formatDate($fn.now(), 'yyyy-MM-dd')");
         testExpression(dataValues, "0", "$fn.monthsSince($lastWeek)");
         testExpression(dataValues, "1", "$fn.monthsSince($lastMonth)");
-        testExpression(dataValues, "13", "$fn.monthsSince($lastYear)");
+        testExpression(dataValues, "12", "$fn.monthsSince($lastYear)");
         testExpression(dataValues, "0", "$fn.daysSince($fn.now())");
         testExpression(dataValues, "1", "$fn.daysSince($yesterday)");
         testExpression(dataValues, "36", "$fn.monthsUntil($in3Years)");
