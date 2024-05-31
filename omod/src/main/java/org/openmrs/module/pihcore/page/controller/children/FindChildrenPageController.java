cfe15edb47c87bdefaa804cd6c0a68c1580c995a
Cosmin <cioan@pih.org>
2024-05-31 10:36:16 -0400

    SL-614: use mother's address when registering a child

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/children/FindChildrenPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/children/FindChildrenPageController.java
@@ -12,13 +12,16 @@ import org.openmrs.module.pihcore.PihEmrConfigConstants;
 import org.openmrs.module.pihcore.metadata.Metadata;
 import org.openmrs.module.reporting.common.ObjectUtil;
 import org.openmrs.ui.framework.SimpleObject;
-import org.openmrs.module.reporting.common.ObjectUtil;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class FindChildrenPageController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     public void controller(PageModel model, UiUtils ui, UiSessionContext uiSessionContext,
                            @RequestParam("patientId") Patient patient,
                            @RequestParam(value="returnUrl", required=false) String returnUrl,
@@ -41,13 +44,22 @@ public class FindChildrenPageController {
                 phoneNumber = attribute.getValue();
             }
         }
+        String patientInfo = patient.getPatientIdentifier().getIdentifier() + " - "
+                + patient.getFamilyName() + ", "
+                + patient.getGivenName() + ", "
+                + patient.getGender() + ", "
+                + patient.getBirthdate().toString();
         // pass the mother's info to the baby registration form as a list of property-value in the following format:
         // SECTION.QUESTION.FIELD , this matches the existing format of the registration form
         SimpleObject initialValues = SimpleObject.create(
                 "demographics.mothersFirstNameLabel.mothersFirstName", patient.getGivenName(),
-                "contactInfo.phoneNumberLabel.phoneNumber", phoneNumber
+                "contactInfo.personAddressQuestion.country", patient.getPersonAddress().getCountry(),
+                "contactInfo.phoneNumberLabel.phoneNumber", phoneNumber,
+                "registerRelationships.relationships_mother.relationship_type", motherToChildRelationshipType.getUuid() + "-A",
+                "registerRelationships.relationships_mother.other_person_uuid", patient.getUuid(),
+                "registerRelationships.relationships_mother.other_person_name", patientInfo,
+                "registerRelationships.relationships_mother.mother-field", patientInfo
                 );
-
         model.addAttribute("initialRegistrationValues", ui.toJson(initialValues));
         model.addAttribute("returnUrl", returnUrl);
     }
