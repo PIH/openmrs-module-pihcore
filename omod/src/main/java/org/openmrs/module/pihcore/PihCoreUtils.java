4471b49e08f2136171935eb0ec2de1b1dd2984a6
Cosmin <cioan@pih.org>
2024-10-16 14:01:50 -0400

    SL-742: implement Newborn Indicators widget (#533)
    * SL-742: implement Newborn Indicators widget

--- omod/src/main/java/org/openmrs/module/pihcore/PihCoreUtils.java
+++ omod/src/main/java/org/openmrs/module/pihcore/PihCoreUtils.java
@@ -2,13 +2,26 @@ package org.openmrs.module.pihcore;
 
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Concept;
+import org.openmrs.Location;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.PatientProgram;
 import org.openmrs.Program;
+import org.openmrs.Visit;
 import org.openmrs.api.ObsService;
 import org.openmrs.api.ProgramWorkflowService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.adt.AdtService;
+import org.openmrs.module.emrapi.adt.InpatientAdmission;
+import org.openmrs.module.emrapi.adt.InpatientAdmissionSearchCriteria;
+import org.openmrs.module.emrapi.adt.InpatientRequest;
+import org.openmrs.module.emrapi.adt.InpatientRequestSearchCriteria;
+import org.openmrs.module.emrapi.disposition.DispositionType;
+import org.openmrs.module.queue.api.QueueServicesWrapper;
+import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
+import org.openmrs.module.queue.model.QueueEntry;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,7 +31,7 @@ import java.util.Set;
 
 public class PihCoreUtils {
 
-    public static List<Obs> getObsWithinProgram(Patient patient, Set<Concept> concepts, String programUuid) {
+    public static List<Obs> getObsWithinProgram(Patient patient, Set<Concept> questions, Set<Concept> answers, String programUuid) {
 
         List<Obs> obsList = null;
         ProgramWorkflowService programWorkflowService = Context.getProgramWorkflowService();
@@ -40,8 +53,8 @@ public class PihCoreUtils {
         obsList = obsService.getObservations(
                 Arrays.asList(patient),
                 null,
-                new ArrayList<>(concepts),
-                null,
+                questions != null ? new ArrayList<>(questions) : null,
+                answers != null ? new ArrayList<>(answers) : null,
                 null,
                 null,
                 Arrays.asList("obsDatetime"), //sort by field
@@ -52,5 +65,79 @@ public class PihCoreUtils {
                 false); //includeVoidedObs
 
         return obsList;
+    }
+
+    public static SimpleObject getInpatientLocation(Patient patient, Visit activeVisit, AdtService adtService, UiUtils ui) {
+        Location currentInpatientLocation = null;
+        String patientStatus = "";
+        String queueName = "";
+        if (activeVisit != null ) {
+            // #1 Check if patient was admitted
+            InpatientAdmissionSearchCriteria criteria = new InpatientAdmissionSearchCriteria();
+            criteria.setPatientIds(Arrays.asList(patient.getPatientId()));
+            criteria.setIncludeDischarged(false);
+            List<InpatientAdmission> inpatientAdmissions = adtService.getInpatientAdmissions(criteria);
+            InpatientRequest currentInpatientRequest = null;
+            if (inpatientAdmissions != null && !inpatientAdmissions.isEmpty()) {
+                for (InpatientAdmission inpatientAdmission : inpatientAdmissions) {
+                    // it should never be more than one inpatientAdmission per patient
+                    currentInpatientLocation = inpatientAdmission.getCurrentInpatientLocation();
+                    currentInpatientRequest = inpatientAdmission.getCurrentInpatientRequest();
+                    if (currentInpatientRequest != null) {
+                        String dispositionLocation = currentInpatientRequest.getDispositionLocation() !=null ?  currentInpatientRequest.getDispositionLocation().getName() : currentInpatientRequest.getDispositionEncounter().getLocation().getName();
+                        if (currentInpatientRequest.getDispositionType() == DispositionType.ADMIT) {
+                            patientStatus = ui.message("pihcore.waiting.tobe.admitted") + " " + dispositionLocation;
+                        } else if (currentInpatientRequest.getDispositionType() == DispositionType.TRANSFER) {
+                            patientStatus = ui.message("pihcore.waiting.for.transfer") + " " + dispositionLocation;
+                        } else if (currentInpatientRequest.getDispositionType() == DispositionType.DISCHARGE) {
+                            patientStatus = ui.message("pihcore.waiting.for.discharge") + " " + dispositionLocation;
+                        } else {
+                            patientStatus = currentInpatientRequest.getDisposition().getName().getName();
+                        }
+                    } else {
+                        patientStatus = ui.message("pihcore.admitted");
+                    }
+                }
+            } else {
+                // #2 Check for disposition requests for this patient
+                InpatientRequestSearchCriteria requestSearchCriteria = new InpatientRequestSearchCriteria();
+                requestSearchCriteria.setPatientIds(Arrays.asList(patient.getPatientId()));
+                requestSearchCriteria.setVisitIds(Arrays.asList(activeVisit.getVisitId()));
+                List<InpatientRequest> inpatientRequests = adtService.getInpatientRequests(requestSearchCriteria);
+                if (inpatientRequests != null && !inpatientRequests.isEmpty()) {
+                    for (InpatientRequest inpatientRequest : inpatientRequests) {
+                        currentInpatientLocation = inpatientRequest.getDispositionLocation() !=null ? inpatientRequest.getDispositionLocation() : inpatientRequest.getDispositionEncounter().getLocation();
+                        if (inpatientRequest.getDispositionType() == DispositionType.ADMIT) {
+                            patientStatus = ui.message("pihcore.waiting.tobe.admitted") + " " + currentInpatientLocation;
+                        } else if (inpatientRequest.getDispositionType() == DispositionType.TRANSFER) {
+                            patientStatus = ui.message("pihcore.waiting.for.transfer") + " " + currentInpatientLocation;
+                        } else if (inpatientRequest.getDispositionType() == DispositionType.DISCHARGE) {
+                            patientStatus = ui.message("pihcore.waiting.for.discharge") + " " + currentInpatientLocation;
+                        } else {
+                            patientStatus = inpatientRequest.getDisposition().getName().getName();
+                        }
+                    }
+                }
+            }
+
+            if (StringUtils.isBlank(patientStatus)) {
+                // #3 Check to see if the patient is waiting on any queues
+                QueueEntrySearchCriteria qeSearchCriteria = new QueueEntrySearchCriteria();
+                qeSearchCriteria.setPatient(patient);
+                qeSearchCriteria.setVisit(activeVisit);
+                qeSearchCriteria.setIsEnded(null);
+                QueueServicesWrapper queueServicesWrapper = Context.getRegisteredComponents(QueueServicesWrapper.class).get(0);
+                List<QueueEntry> queueEntries = queueServicesWrapper.getQueueEntryService().getQueueEntries(qeSearchCriteria);
+                for (QueueEntry queueEntry : queueEntries) {
+                    queueName = queueEntry.getQueue().getName();
+                    patientStatus = queueEntry.getStatus().getName().getName();
+                }
+            }
+        }
+        return SimpleObject.create(
+                "currentInpatientLocation", currentInpatientLocation,
+                "queueName", queueName,
+                "patientStatus", patientStatus
+        );
     }
 }
--- omod/src/main/java/org/openmrs/module/pihcore/fragment/controller/dashboardwidgets/PatientLocationFragmentController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/fragment/controller/dashboardwidgets/PatientLocationFragmentController.java
@@ -1,24 +1,15 @@
 package org.openmrs.module.pihcore.fragment.controller.dashboardwidgets;
 
 
-import org.apache.commons.lang.StringUtils;
-import org.openmrs.Location;
 import org.openmrs.Patient;
 import org.openmrs.api.PatientService;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.appframework.domain.AppDescriptor;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.adt.AdtService;
-import org.openmrs.module.emrapi.adt.InpatientAdmission;
-import org.openmrs.module.emrapi.adt.InpatientAdmissionSearchCriteria;
-import org.openmrs.module.emrapi.adt.InpatientRequest;
-import org.openmrs.module.emrapi.adt.InpatientRequestSearchCriteria;
-import org.openmrs.module.emrapi.disposition.DispositionType;
 import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
 import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
-import org.openmrs.module.queue.api.QueueServicesWrapper;
-import org.openmrs.module.queue.api.search.QueueEntrySearchCriteria;
-import org.openmrs.module.queue.model.QueueEntry;
+import org.openmrs.module.pihcore.PihCoreUtils;
+import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.annotation.SpringBean;
@@ -26,9 +17,6 @@ import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class PatientLocationFragmentController {
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -64,76 +52,12 @@ public class PatientLocationFragmentController {
             throw new IllegalArgumentException("No patient found. Please pass patient into the configuration");
         }
         VisitDomainWrapper activeVisit = adtService.getActiveVisit(patient, uiSessionContext.getSessionLocation());
-        String patientStatus = "";
-        String queueName = "";
-        Location currentInpatientLocation = null;
-        if (activeVisit != null ) {
-            // #1 Check if patient was admitted
-            InpatientAdmissionSearchCriteria criteria = new InpatientAdmissionSearchCriteria();
-            criteria.setPatientIds(Arrays.asList(patient.getPatientId()));
-            criteria.setIncludeDischarged(false);
-            List<InpatientAdmission> inpatientAdmissions = adtService.getInpatientAdmissions(criteria);
-            InpatientRequest currentInpatientRequest = null;
-            if (inpatientAdmissions != null && !inpatientAdmissions.isEmpty()) {
-                for (InpatientAdmission inpatientAdmission : inpatientAdmissions) {
-                    // it should never be more than one inpatientAdmission per patient
-                    currentInpatientLocation = inpatientAdmission.getCurrentInpatientLocation();
-                    currentInpatientRequest = inpatientAdmission.getCurrentInpatientRequest();
-                    if (currentInpatientRequest != null) {
-                        String dispositionLocation = currentInpatientRequest.getDispositionLocation() !=null ?  currentInpatientRequest.getDispositionLocation().getName() : currentInpatientRequest.getDispositionEncounter().getLocation().getName();
-                        if (currentInpatientRequest.getDispositionType() == DispositionType.ADMIT) {
-                            patientStatus = ui.message("pihcore.waiting.tobe.admitted") + " " + dispositionLocation;
-                        } else if (currentInpatientRequest.getDispositionType() == DispositionType.TRANSFER) {
-                            patientStatus = ui.message("pihcore.waiting.for.transfer") + " " + dispositionLocation;
-                        } else if (currentInpatientRequest.getDispositionType() == DispositionType.DISCHARGE) {
-                            patientStatus = ui.message("pihcore.waiting.for.discharge") + " " + dispositionLocation;
-                        } else {
-                            patientStatus = currentInpatientRequest.getDisposition().getName().getName();
-                        }
-                    } else {
-                        patientStatus = ui.message("pihcore.admitted");
-                    }
-                }
-            } else {
-                // #2 Check for disposition requests for this patient
-                InpatientRequestSearchCriteria requestSearchCriteria = new InpatientRequestSearchCriteria();
-                requestSearchCriteria.setPatientIds(Arrays.asList(patient.getPatientId()));
-                requestSearchCriteria.setVisitIds(Arrays.asList(activeVisit.getVisitId()));
-                List<InpatientRequest> inpatientRequests = adtService.getInpatientRequests(requestSearchCriteria);
-                if (inpatientRequests != null && !inpatientRequests.isEmpty()) {
-                    for (InpatientRequest inpatientRequest : inpatientRequests) {
-                        currentInpatientLocation = inpatientRequest.getDispositionLocation() !=null ? inpatientRequest.getDispositionLocation() : inpatientRequest.getDispositionEncounter().getLocation();
-                        if (inpatientRequest.getDispositionType() == DispositionType.ADMIT) {
-                            patientStatus = ui.message("pihcore.waiting.tobe.admitted") + " " + currentInpatientLocation;
-                        } else if (inpatientRequest.getDispositionType() == DispositionType.TRANSFER) {
-                            patientStatus = ui.message("pihcore.waiting.for.transfer") + " " + currentInpatientLocation;
-                        } else if (inpatientRequest.getDispositionType() == DispositionType.DISCHARGE) {
-                            patientStatus = ui.message("pihcore.waiting.for.discharge") + " " + currentInpatientLocation;
-                        } else {
-                            patientStatus = inpatientRequest.getDisposition().getName().getName();
-                        }
-                    }
-                }
-            }
+        SimpleObject inpatientLocation = PihCoreUtils.getInpatientLocation(patient, activeVisit != null ?  activeVisit.getVisit() : null, adtService, ui);
 
-            if (StringUtils.isBlank(patientStatus)) {
-                // #3 Check to see if the patient is waiting on any queues
-                QueueEntrySearchCriteria qeSearchCriteria = new QueueEntrySearchCriteria();
-                qeSearchCriteria.setPatient(patient);
-                qeSearchCriteria.setVisit(activeVisit.getVisit());
-                qeSearchCriteria.setIsEnded(null);
-                QueueServicesWrapper queueServicesWrapper = Context.getRegisteredComponents(QueueServicesWrapper.class).get(0);
-                List<QueueEntry> queueEntries = queueServicesWrapper.getQueueEntryService().getQueueEntries(qeSearchCriteria);
-                for (QueueEntry queueEntry : queueEntries) {
-                    queueName = queueEntry.getQueue().getName();
-                    patientStatus = queueEntry.getStatus().getName().getName();
-                }
-            }
-        }
         model.put("patient", patient);
         model.put("app", app);
-        model.put("patientStatus", patientStatus);
-        model.put("inpatientLocation", currentInpatientLocation);
-        model.put("queueName", queueName);
+        model.put("patientStatus", inpatientLocation.get("patientStatus"));
+        model.put("inpatientLocation", inpatientLocation.get("currentInpatientLocation"));
+        model.put("queueName", inpatientLocation.get("queueName"));
     }
 }
