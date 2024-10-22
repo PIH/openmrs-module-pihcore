90ecc85a6c5bc4d4f41bd1174eb22ace50564570
Mark Goodrich <mark.goodrich@live.com>
2024-10-22 17:39:10 -0400

    SL-786: Set Outcome to "Treatment Completed" when Treatment Status is… (#536)

--- api/src/main/java/org/openmrs/module/pihcore/task/ClosePregnancyProgramTask.java
+++ api/src/main/java/org/openmrs/module/pihcore/task/ClosePregnancyProgramTask.java
@@ -18,6 +18,7 @@ import java.util.List;
 /**
  * Patients should be unenrolled automatically from the pregnancy program if they have been in the Antenatal Treatment State for 11 months. The program outcome should be Lost to Follow Up.
  * Patients should be unenrolled automatically from the pregnancy program 6 weeks after the Delivered treatment state start date with an outcome of Treatment Completed
+ * For any programs with a completion date within the last year, if the final state is Miscarried and there is no outcome, the outcome should be set to Treatment Completed
  */
 public class ClosePregnancyProgramTask implements Runnable {
 
@@ -45,8 +46,9 @@ public class ClosePregnancyProgramTask implements Runnable {
                 ProgramWorkflow treatmentStatusWorkflow = Context.getProgramWorkflowService().getWorkflowByUuid(PihEmrConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_UUID);
                 ProgramWorkflowState antenatalState = Context.getProgramWorkflowService().getStateByUuid(PihEmrConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_ANTENATAL_UUID);
                 ProgramWorkflowState postpartumState = Context.getProgramWorkflowService().getStateByUuid(PihEmrConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_POSTPARTUM_UUID);
+                ProgramWorkflowState miscarriedState = Context.getProgramWorkflowService().getStateByUuid(PihEmrConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_MISCARRIED_UUID);
                 Concept treatmentCompleteConcept = Context.getConceptService().getConceptByMapping("1714", "PIH");
-                Concept lostToFollowup = Context.getConceptService().getConceptByMapping("5240", "PIH");
+                Concept lostToFollowupConcept = Context.getConceptService().getConceptByMapping("5240", "PIH");
                 Date sixWeeksAgo = new DateTime().minusWeeks(6).toDate();
                 Date elevenMonthsAgo = new DateTime().minusMonths(11).toDate();
 
@@ -62,7 +64,21 @@ public class ClosePregnancyProgramTask implements Runnable {
                                 Context.getProgramWorkflowService().savePatientProgram(patientPregnancyProgram);
                             } else if (currentState.getState().equals(antenatalState) && currentState.getStartDate().before(elevenMonthsAgo)) {
                                 patientPregnancyProgram.setDateCompleted(new Date());
-                                patientPregnancyProgram.setOutcome(lostToFollowup);
+                                patientPregnancyProgram.setOutcome(lostToFollowupConcept);
+                                Context.getProgramWorkflowService().savePatientProgram(patientPregnancyProgram);
+                            }
+                        }
+                    }
+                }
+
+                // now fetch all pregnancy programs that *have* been completed within the last year, and if their final state is miscarried, set the outcome to treatment complete
+                // note: when back entering information from more than a year ago, this will not work as expected, as for performance reasons it will only look at the last year
+                patientPregnancyPrograms = Context.getProgramWorkflowService().getPatientPrograms(null, pregnancyProgram, null, null, new DateTime().minusYears(1).toDate(), new Date(), true);
+                for (PatientProgram patientPregnancyProgram : patientPregnancyPrograms) {
+                    if (patientPregnancyProgram.getDateCompleted() != null && patientPregnancyProgram.getOutcome() == null) {
+                        for (PatientState patientState : patientPregnancyProgram.getMostRecentStateInEachWorkflow()) {
+                            if (patientState.getState().equals(miscarriedState)) {
+                                patientPregnancyProgram.setOutcome(treatmentCompleteConcept);
                                 Context.getProgramWorkflowService().savePatientProgram(patientPregnancyProgram);
                             }
                         }
