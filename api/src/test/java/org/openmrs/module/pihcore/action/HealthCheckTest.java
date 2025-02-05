52b44ff7d6cc70e3ec993c45926b8b420de0858b
Michael Seaton <mseaton@pih.org>
2025-02-05 14:51:29 -0500

    UHM-8303 - simplify

---
+++ api/src/test/java/org/openmrs/module/pihcore/action/HealthCheckTest.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.pihcore.action;
+
+import org.junit.jupiter.api.Test;
+import org.openmrs.test.jupiter.BaseModuleContextSensitiveTest;
+
+public class HealthCheckTest extends BaseModuleContextSensitiveTest {
+
+    @Test
+    public void reopenVisitAction_shouldReopenVisitIfNoSubsequentVisitAtSameLocation() {
+        System.out.println("EXECUTING TEST");
+    }
+}
