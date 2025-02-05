cc4d94d2375d47df389f06a19ae6e03e618d86b5
Michael Seaton <mseaton@pih.org>
2025-02-05 17:54:29 -0500

    Additional dbunit logging

--- api/src/test/java/org/openmrs/module/pihcore/action/HealthCheckTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/action/HealthCheckTest.java
@@ -11,7 +11,7 @@ public class HealthCheckTest extends BaseModuleContextSensitiveTest {
     @Override
     public Properties getRuntimeProperties() {
         runtimeProperties = super.getRuntimeProperties();
-        String url = "jdbc:h2:mem:openmrs;DB_CLOSE_DELAY=30;LOCK_TIMEOUT=10000;IGNORECASE=TRUE;DATABASE_TO_LOWER=TRUE;CASE_INSENSITIVE_IDENTIFIERS=TRUE";
+        String url = "jdbc:h2:mem:openmrs;DB_CLOSE_DELAY=30;LOCK_TIMEOUT=10000;IGNORECASE=TRUE;DATABASE_TO_LOWER=TRUE;CASE_INSENSITIVE_IDENTIFIERS=TRUE;TRACE_LEVEL_FILE=3;TRACE_LEVEL_SYSTEM_OUT=3";
         runtimeProperties.setProperty(Environment.URL, url);
         runtimeProperties.setProperty("connection.url", url);
         return runtimeProperties;
