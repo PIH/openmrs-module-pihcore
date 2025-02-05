1d7efc3d1b4ceb66499d13a4b6cbbfe53eacbd8f
Michael Seaton <mseaton@pih.org>
2025-02-05 17:29:38 -0500

    Tweak url for lower case

--- api/src/test/java/org/openmrs/module/pihcore/action/HealthCheckTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/action/HealthCheckTest.java
@@ -1,9 +1,21 @@
 package org.openmrs.module.pihcore.action;
 
+import org.hibernate.cfg.Environment;
 import org.junit.jupiter.api.Test;
 import org.openmrs.test.jupiter.BaseModuleContextSensitiveTest;
 
+import java.util.Properties;
+
 public class HealthCheckTest extends BaseModuleContextSensitiveTest {
+
+    @Override
+    public Properties getRuntimeProperties() {
+        runtimeProperties = super.getRuntimeProperties();
+        String url = "jdbc:h2:mem:openmrs;DB_CLOSE_DELAY=30;LOCK_TIMEOUT=10000;IGNORECASE=TRUE;DATABASE_TO_LOWER=TRUE;CASE_INSENSITIVE_IDENTIFIERS=TRUE";
+        runtimeProperties.setProperty(Environment.URL, url);
+        runtimeProperties.setProperty("connection.url", url);
+        return runtimeProperties;
+    }
 
     @Test
     public void reopenVisitAction_shouldReopenVisitIfNoSubsequentVisitAtSameLocation() {
