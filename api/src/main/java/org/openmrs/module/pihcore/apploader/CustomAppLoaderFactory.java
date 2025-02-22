223bc3763b7d920ae8a6d85086683d45cfca152c
louidorjp <jpaullouidor@gmail.com>
2025-02-21 22:02:10 -0500

    HAI-1024-update:  breadcrumbs now correctly navigate back to patient dashboard from full lab results

--- api/src/main/java/org/openmrs/module/pihcore/apploader/CustomAppLoaderFactory.java
+++ api/src/main/java/org/openmrs/module/pihcore/apploader/CustomAppLoaderFactory.java
@@ -2820,7 +2820,7 @@ public class CustomAppLoaderFactory implements AppFrameworkFactory {
                         null,
                         objectNode(
                                 "encounterTypes", Context.getAdministrationService().getGlobalProperty(CustomAppLoaderConstants.GP_LABWORKFLOW_OWA_LABRESULTSENCOUNTERTYPES),
-                                "detailsUrl", "owa/labworkflow/index.html?patient={{patient.uuid}}&returnUrl=" + "/" + WebConstants.CONTEXT_PATH + "/coreapps/clinicianfacing/patient.page?patientId={{patient.uuid}}#/LabResults",
+                                "detailsUrl", "owa/labworkflow/index.html?patient={{patient.uuid}}&returnUrl=" + "/coreapps/clinicianfacing/patient.page?patientId={{patient.uuid}}#/LabResults",
                                 "obsTypes", "LabSet,Test",
                                 "labCategoriesSet", PihEmrConfigConstants.CONCEPT_LABORATORYCATEGORIES_UUID,
                                 "maxToDisplay", "5"
