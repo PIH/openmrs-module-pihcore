861032f06d9152bb734021cf54d6932c2a066775
Michael Seaton <mseaton@pih.org>
2025-07-17 17:13:28 -0400

    UHM-8649 - Do not whitelist rest urls, use default authentication setup

--- api/src/main/java/org/openmrs/module/pihcore/setup/AuthenticationSetup.java
+++ api/src/main/java/org/openmrs/module/pihcore/setup/AuthenticationSetup.java
@@ -15,7 +15,6 @@ import static org.openmrs.module.authentication.AuthenticationConfig.SCHEME;
 import static org.openmrs.module.authentication.AuthenticationConfig.SCHEME_CONFIG_PREFIX_TEMPLATE;
 import static org.openmrs.module.authentication.AuthenticationConfig.SCHEME_ID;
 import static org.openmrs.module.authentication.AuthenticationConfig.SCHEME_TYPE_TEMPLATE;
-import static org.openmrs.module.authentication.AuthenticationConfig.WHITE_LIST;
 
 public class AuthenticationSetup {
 
@@ -43,28 +42,6 @@ public class AuthenticationSetup {
         String scheme = StringUtils.isBlank(cd.getScheme()) ? BASIC : cd.getScheme();
         AuthenticationConfig.setProperty(SCHEME, scheme);
 
-        // We set up white list as everything needed for the basic login page and any additional scheme login page
-        // Add in any additional white list pages that are included in the config
-
-        Set<String> whitelist = new TreeSet<>(cd.getWhitelist());
-        whitelist.add("/login.htm");
-        whitelist.add("/authenticationui/login/login.page");
-        whitelist.add("/appui/session/getLoginLocations.action");
-        whitelist.add("/authenticationui/resetPassword/reset.action");
-        whitelist.add("/authenticationui/account/resetPassword.page");
-        whitelist.add("/ws/rest/v1/session");
-        whitelist.add("/csrfguard");
-        whitelist.add("/spa/**/*");
-        whitelist.add("*.js");
-        whitelist.add("*.css");
-        whitelist.add("*.gif");
-        whitelist.add("*.jpg");
-        whitelist.add("*.png");
-        whitelist.add("*.ico");
-        whitelist.add("*.ttf");
-        whitelist.add("*.woff");
-        whitelist.add("*.woff2");
-
         // Set up all the supported authentication schemes with default values.
         // Allow overriding with values from the config
 
@@ -80,7 +57,7 @@ public class AuthenticationSetup {
             p.put("onlyLocationsWithTag", "Login Location");
             p.put("locationSessionAttributeName", "emrContext.sessionLocationId");
             p.put("lastLocationCookieName", "emr.lastSessionLocation");
-            addScheme(BASIC, className, p, whitelist);
+            addScheme(BASIC, className, p);
         }
 
         // Secret Question Authentication Scheme.  This is an available 2nd factor
@@ -89,7 +66,7 @@ public class AuthenticationSetup {
             Properties p = new Properties();
             p.put("loginPage", "/authenticationui/login/loginSecret.page");
             p.put("configurationPage", "/authenticationui/account/changeSecurityQuestion.page?schemeId={schemeId}&userId={userId}");
-            addScheme(SECRET, className, p, whitelist);
+            addScheme(SECRET, className, p);
         }
 
         // Totp Authentication Scheme.  This is an available 2nd factor
@@ -99,7 +76,7 @@ public class AuthenticationSetup {
             p.put("qrCodeIssuer", "PIHEMR");
             p.put("loginPage", "/authenticationui/login/loginTotp.page");
             p.put("configurationPage", "/authenticationui/account/configureTotp.page?schemeId={schemeId}&userId={userId}");
-            addScheme(TOTP, className, p, whitelist);
+            addScheme(TOTP, className, p);
         }
 
         // Two-Factor Authentication Scheme.
@@ -108,7 +85,7 @@ public class AuthenticationSetup {
             Properties p = new Properties();
             p.put("primaryOptions", BASIC);
             p.put("secondaryOptions", SECRET + "," + TOTP);
-            addScheme(TWO_FACTOR, className, p, whitelist);
+            addScheme(TWO_FACTOR, className, p);
         }
 
         // Configuration overrides
@@ -121,13 +98,8 @@ public class AuthenticationSetup {
                 String propertyName = SCHEME_CONFIG_PREFIX_TEMPLATE.replace(SCHEME_ID, schemeId) + property;
                 String propertyVal = sd.getConfig().get(property);
                 AuthenticationConfig.setProperty(propertyName, propertyVal);
-                if (property.equalsIgnoreCase("loginPage")) {
-                    whitelist.add(propertyVal);
-                }
             }
         }
-
-        AuthenticationConfig.setProperty(WHITE_LIST, String.join(",", whitelist));
 
         log.info("Authentication Schemes Configured");
         Properties p = AuthenticationConfig.getConfig();
@@ -140,7 +112,7 @@ public class AuthenticationSetup {
     /**
      * Add configuration for a scheme with the given schemeId
      */
-    protected static void addScheme(String schemeId, String className, Properties config, Set<String> whitelist) {
+    protected static void addScheme(String schemeId, String className, Properties config) {
         String schemeTypeProperty = SCHEME_TYPE_TEMPLATE.replace(SCHEME_ID, schemeId);
         AuthenticationConfig.setProperty(schemeTypeProperty, className);
         if (config != null) {
@@ -148,9 +120,6 @@ public class AuthenticationSetup {
                 String key = SCHEME_CONFIG_PREFIX_TEMPLATE.replace(SCHEME_ID, schemeId) + propertyName;
                 String value = config.getProperty(propertyName);
                 AuthenticationConfig.setProperty(key, value);
-                if (propertyName.equalsIgnoreCase("loginPage")) {
-                    whitelist.add(value);
-                }
             }
         }
     }
