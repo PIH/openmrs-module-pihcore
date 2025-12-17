58bc4943462374d79959a2a6ae9ad5de43cbeae9
Cosmin <cioan@pih.org>
2025-12-17 09:03:04 -0500

    SL-1129: handle pregnancy program states when Postnatal encounter is entered (#617)

--- api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/PregnancyProgramEnrollmentAction.java
+++ api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/PregnancyProgramEnrollmentAction.java
@@ -2,9 +2,12 @@ package org.openmrs.module.pihcore.htmlformentry.action;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
 import org.openmrs.Encounter;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.PatientProgram;
+import org.openmrs.PatientState;
 import org.openmrs.Program;
 import org.openmrs.ProgramWorkflowState;
 import org.openmrs.api.context.Context;
@@ -13,10 +16,12 @@ import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.openmrs.module.pihcore.SierraLeoneConfigConstants;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 import static org.openmrs.module.pihcore.htmlformentry.action.util.PregnancyProgramActionUtils.endEnrollment;
 import static org.openmrs.module.pihcore.htmlformentry.action.util.PregnancyProgramActionUtils.enrollInPregnancyProgram;
+import static org.openmrs.module.pihcore.htmlformentry.action.util.PregnancyProgramActionUtils.getTypeOfTreatmentCurrentState;
 import static org.openmrs.module.pihcore.htmlformentry.action.util.PregnancyProgramActionUtils.getTypeOfTreatmentStateOnDate;
 
 /**
@@ -32,11 +37,13 @@ import static org.openmrs.module.pihcore.htmlformentry.action.util.PregnancyProg
 public class PregnancyProgramEnrollmentAction implements CustomFormSubmissionAction {
 
     protected final Log log = LogFactory.getLog(getClass());
+    private static final String DELIVERY_DATE_CONCEPT_MAPPING = "5599";
 
     @Override
     public void applyAction(FormEntrySession formEntrySession) {
         Program pregnancyProgram = Context.getProgramWorkflowService().getProgramByUuid(SierraLeoneConfigConstants.PROGRAM_PREGNANCY_UUID);
         ProgramWorkflowState antenatalState = Context.getProgramWorkflowService().getStateByUuid(SierraLeoneConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_ANTENATAL_UUID);
+        ProgramWorkflowState postpartumState = Context.getProgramWorkflowService().getStateByUuid(SierraLeoneConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_POSTPARTUM_UUID);
 
         Patient patient = formEntrySession.getPatient();
         Encounter encounter = formEntrySession.getEncounter();
@@ -47,20 +54,49 @@ public class PregnancyProgramEnrollmentAction implements CustomFormSubmissionAct
         List<PatientProgram> activePatientPregnancyPrograms = patientPregnancyPrograms.stream().filter(patientProgram -> patientProgram.getActive(encounter.getEncounterDatetime())).collect(Collectors.toList());
 
         if (activePatientPregnancyPrograms.isEmpty()) {
-            // enroll if no active program
-            enrollInPregnancyProgram(patient, antenatalState, encounter.getEncounterDatetime(), encounter.getLocation(), patientPregnancyPrograms);
+            if (encounter.getEncounterType() != null && encounter.getEncounterType().getUuid().equals(SierraLeoneConfigConstants.ENCOUNTERTYPE_POSTNATALFOLLOWUP_UUID)){
+                enrollInPregnancyProgram(patient, postpartumState, encounter.getEncounterDatetime(), encounter.getLocation(), patientPregnancyPrograms);
+            } else {
+                // enroll if no active program
+                enrollInPregnancyProgram(patient, antenatalState, encounter.getEncounterDatetime(), encounter.getLocation(), patientPregnancyPrograms);
+            }
         } else {
             if (activePatientPregnancyPrograms.size() > 1) {
                 log.warn("Patient " + patient.getUuid() + " is enrolled in multiple active pregnancy programs, likely a data error. Operating on the most recent one.");
             }
-            // if the patient is enrolled, but in postpartum state, end the current enrollment and enroll in new program
+
             PatientProgram activePregnancyProgram = activePatientPregnancyPrograms.get(activePatientPregnancyPrograms.size() - 1);
-            if (SierraLeoneConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_POSTPARTUM_UUID
+            if (encounter.getEncounterType() != null && encounter.getEncounterType().getUuid().equals(SierraLeoneConfigConstants.ENCOUNTERTYPE_POSTNATALFOLLOWUP_UUID)) {
+                PatientState patientStateCurrent = getTypeOfTreatmentCurrentState(activePregnancyProgram.getStates()).orElse(null);
+                if (patientStateCurrent.getState().getUuid().equals(SierraLeoneConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_ANTENATAL_UUID)) {
+                    //since this is a Postnatal Followup encounter, and the active program state is ANTENATAL we need to transition to POSTPARTUM state
+                    Date deliveryDate = getDeliveryDate(encounter); // first look for Delivery date obs
+                    activePregnancyProgram.transitionToState(postpartumState, deliveryDate);
+                    Context.getProgramWorkflowService().savePatientProgram(activePregnancyProgram);
+                }
+            } else if (SierraLeoneConfigConstants.PROGRAMWORKFLOW_PREGNANCYPROGRAMTYPEOFTREATMENT_STATE_POSTPARTUM_UUID
                     .equals(getTypeOfTreatmentStateOnDate(activePregnancyProgram.getStates(), encounter.getEncounterDatetime()).map(patientState -> patientState.getState().getUuid()).orElse(null))) {
+                // if the patient is enrolled, but in postpartum state, end the current enrollment and enroll in new program
                 endEnrollment(activePregnancyProgram, encounter);
                 enrollInPregnancyProgram(patient, antenatalState, encounter.getEncounterDatetime(), encounter.getLocation(), patientPregnancyPrograms);
             }
             // otherwise, do nothing
         }
+    }
+
+    private Date getDeliveryDate(Encounter encounter) {
+        Concept concept = Context.getConceptService().getConceptByMapping(DELIVERY_DATE_CONCEPT_MAPPING, "PIH");
+        Date deliveryDate = null;
+        // look for any delivery date concepts and get the latest one
+        for (Obs obs : encounter.getAllFlattenedObs(false)) {
+            if (obs.getConcept().equals(concept)) {
+                if (deliveryDate == null || obs.getValueDatetime().after(deliveryDate)) {
+                    deliveryDate = obs.getValueDatetime();
+                }
+            }
+        }
+
+        // if no delivery date concept found, use the encounter date
+        return deliveryDate != null ? deliveryDate : encounter.getEncounterDatetime();
     }
 }
