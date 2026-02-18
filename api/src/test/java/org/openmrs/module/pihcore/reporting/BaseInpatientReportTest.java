f3ed15553d37cac4fc8c4a37d98483f2f18b2eb8
mogoodrich <mgoodrich@pih.org>
2026-02-18 15:42:46 -0500

    fix test intialization after addition of EMR API ADT validator

--- api/src/test/java/org/openmrs/module/pihcore/reporting/BaseInpatientReportTest.java
+++ api/src/test/java/org/openmrs/module/pihcore/reporting/BaseInpatientReportTest.java
@@ -11,6 +11,7 @@ import org.openmrs.VisitType;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.pihcore.PihEmrConfigConstants;
 import org.openmrs.test.SkipBaseSetup;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,87 +76,99 @@ public abstract class BaseInpatientReportTest extends BaseReportTest {
         patient2 = data.randomPatient().save();
         startDateStr = "2013-10-01 17:30:00";
         patient2 = this.conditionallySetBirthDateBeforeStartDate(patient2, startDateStr);
-        Visit visit2 = data.visit().patient(patient2).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-03 12:45:00")
-                .location(visitLocation).save();
-        data.encounter().visit(visit2).encounterType(checkIn)
+
+        Encounter enc2a = data.encounter().encounterType(checkIn).patient(patient2)
                 .location(outpatientClinic).encounterDatetime(startDateStr).save();
-        data.encounter().visit(visit2).encounterType(admission)
+        Encounter enc2b = data.encounter().encounterType(admission).patient(patient2)
                 .location(womensInternalMedicine).encounterDatetime("2013-10-01 18:30:00").save();
-        data.encounter().visit(visit2).encounterType(exit)
+        Encounter enc2c = data.encounter().encounterType(exit).patient(patient2)
                 .location(womensInternalMedicine).encounterDatetime("2013-10-02 23:45:00").save();
+
+        data.visit().patient(patient2).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-03 12:45:00")
+                .location(visitLocation).encounter(enc2a).encounter(enc2b).encounter(enc2c).save();
 
         // Admitted during the day of 3 Oct (Men's Internal Medicine)
         patient3 = data.randomPatient().save();
         startDateStr = "2013-10-03 12:34:00";
         patient3 = this.conditionallySetBirthDateBeforeStartDate(patient3, startDateStr);
-        Visit visit3a = data.visit().patient(patient3).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-07 12:45:00")
-                .location(visitLocation).save();
-        data.encounter().visit(visit3a).encounterType(checkIn)
+
+        Encounter enc3a  = data.encounter().patient(patient3).encounterType(checkIn)
                 .location(outpatientClinic).encounterDatetime(startDateStr).save();
-        data.encounter().visit(visit3a).encounterType(admission)
+        Encounter enc3b = data.encounter().patient(patient3).encounterType(admission)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-03 13:57:00").save();
-        Encounter encounter3a = data.encounter().visit(visit3a).encounterType(exit)
+        Encounter enc3c = data.encounter().patient(patient3).encounterType(exit)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-07 12:45:00").save();
-        data.obs().encounter(encounter3a).concept(dispositionConcept).value("Transfer out of hospital", "PIH").save();
+        data.obs().encounter(enc3c).concept(dispositionConcept).value("Transfer out of hospital", "PIH").save();
+
+        data.visit().patient(patient3).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-07 12:45:00")
+                .location(visitLocation).encounter(enc3a).encounter(enc3b).encounter(enc3c).save();
 
         // This was a possible readmission, because the patient was admitted and left the previous day
         startDateStr = "2013-10-02 12:34:00";
-        Visit visit3b = data.visit().patient(patient3).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-02 16:45:00")
-                .location(visitLocation).save();
-        data.encounter().visit(visit3b).encounterType(admission)
+
+        Encounter enc3d = data.encounter().patient(patient3).encounterType(admission)
                 .location(mensInternalMedicine).encounterDatetime(startDateStr).save();
-        Encounter encounter3b = data.encounter().visit(visit3b).encounterType(exit)
+        Encounter enc3e = data.encounter().patient(patient3).encounterType(exit)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-02 16:45:00").save();
-        data.obs().encounter(encounter3b).concept(dispositionConcept).value("Departed without medical discharge", "PIH").save();
+        data.obs().encounter(enc3e).concept(dispositionConcept).value("Departed without medical discharge", "PIH").save();
+
+        data.visit().patient(patient3).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-02 16:45:00")
+                .location(visitLocation).encounter(enc3d).encounter(enc3e).save();
 
         // Admitted earlier (Men's Internal Medicine), and discharged on 3 Oct
         patient4 = data.randomPatient().save();
         startDateStr = "2013-10-01 12:34:00";
         patient4 = this.conditionallySetBirthDateBeforeStartDate(patient4, startDateStr);
-        Visit visit4 = data.visit().patient(patient4).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-03 12:45:00")
-                .location(visitLocation).save();
-        data.encounter().visit(visit4).encounterType(checkIn)
+
+        Encounter enc4a = data.encounter().patient(patient4).encounterType(checkIn)
                 .location(outpatientClinic).encounterDatetime(startDateStr).save();
-        data.encounter().visit(visit4).encounterType(admission)
+        Encounter enc4b = data.encounter().patient(patient4).encounterType(admission)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-01 13:57:00").save();
-        data.encounter().visit(visit4).encounterType(exit)
+        Encounter enc4c = data.encounter().patient(patient4).encounterType(exit)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-03 12:45:00").save();
         // consult with discharge
-        Encounter encounter4a = data.encounter().visit(visit4).encounterType(consultation)
+        Encounter enc4d = data.encounter().patient(patient4).encounterType(consultation)
                 .location(mensInternalMedicine).encounterDatetime("2013-10-03 12:15:00").save();
-        data.obs().encounter(encounter4a).concept(dispositionConcept).value("DISCHARGED", "PIH").save();
+        data.obs().encounter(enc4d).concept(dispositionConcept).value("DISCHARGED", "PIH").save();
+
+        data.visit().patient(patient4).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-03 12:45:00")
+                .location(visitLocation).encounter(enc4a).encounter(enc4b).encounter(enc4c).encounter(enc4d).save();
 
         // Begins the day of 3 Oct at Women's Inpatient (was admitted earlier), and transferred to Surgical Ward during the day
         patient5 = data.randomPatient().save();
         startDateStr = "2013-10-01 12:34:00";
         patient5 = this.conditionallySetBirthDateBeforeStartDate(patient5, startDateStr);
-        Visit visit5 = data.visit().patient(patient5).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-13 12:45:00")
-                .location(visitLocation).save();
-        data.encounter().visit(visit5).encounterType(checkIn)
+
+        Encounter enc5a = data.encounter().patient(patient5).encounterType(checkIn)
                 .location(outpatientClinic).encounterDatetime(startDateStr).save();
-        data.encounter().visit(visit5).encounterType(admission)
+        Encounter enc5b = data.encounter().patient(patient5).encounterType(admission)
                 .location(womensInternalMedicine).encounterDatetime("2013-10-01 13:57:00").save();
-        data.encounter().visit(visit5).encounterType(transfer)
+        Encounter enc5c = data.encounter().patient(patient5).encounterType(transfer)
                 .location(surgicalWard).encounterDatetime("2013-10-03 12:45:00").save();
+
+        data.visit().patient(patient5).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-13 12:45:00")
+                .location(visitLocation).encounter(enc5a).encounter(enc5b).encounter(enc5c).save();
 
         //  Checks into ED during the day, transferred to surgery (with no admission), and has surgery
         patient6 = data.randomPatient().save();
         startDateStr = "2013-10-03 10:05:00";
         patient6 = this.conditionallySetBirthDateBeforeStartDate(patient6, startDateStr);
-        Visit visit6 = data.visit().patient(patient6).visitType(atFacility)
-                .started(startDateStr).stopped("2013-10-03 18:32:21")
-                .location(visitLocation).save();
-        data.encounter().visit(visit6).encounterType(checkIn)
+
+        Encounter enc6a = data.encounter().patient(patient6).encounterType(checkIn)
                 .location(emergencyDepartment).encounterDatetime(startDateStr).save();
-        data.encounter().visit(visit6).encounterType(transfer)
+        Encounter enc6b =data.encounter().patient(patient6).encounterType(transfer)
                 .location(surgicalWard).encounterDatetime("2013-10-03 12:45:00").save();
-        data.encounter().visit(visit6).encounterType(postOpNote)
+        Encounter enc6c = data.encounter().patient(patient6).encounterType(postOpNote)
                 .location(surgicalWard).encounterDatetime("2013-10-03 14:53:00").save();
+
+        data.visit().patient(patient6).visitType(atFacility)
+                .started(startDateStr).stopped("2013-10-03 18:32:21")
+                .location(visitLocation).encounter(enc6a).encounter(enc6b).encounter(enc6c).save();
     }
 
     // modify birth date of test patient when it is after start date, to avoid
