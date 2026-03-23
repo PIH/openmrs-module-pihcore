047c2e0f366afd2d2945406d1afda5c06e0e4821
mogoodrich <mgoodrich@pih.org>
2026-03-23 10:08:46 -0400

    UHM-9194: Active Visits List should only should visits associated with current facility

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/reports/ActiveVisitsListPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/reports/ActiveVisitsListPageController.java
@@ -3,16 +3,19 @@ package org.openmrs.module.pihcore.page.controller.reports;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.db.hibernate.DbSessionFactory;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.coreapps.CoreAppsConstants;
 import org.openmrs.module.coreapps.CoreAppsProperties;
+import org.openmrs.module.emrapi.adt.AdtService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
 import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,37 +28,28 @@ public class ActiveVisitsListPageController {
     private final Log log = LogFactory.getLog(getClass());
 
     public void get(PageModel model,
+                    @SpringBean AdtService adtService,
                     @SpringBean CoreAppsProperties coreAppsProperties,
-                    @RequestParam(value = "location", required = false) Integer location,
-                    @RequestParam(value = "timeQualifier", required = false) String timeQualifier,
+                    @RequestParam(value = "locationId", required = false) Integer locationId,
                     UiSessionContext uiSessionContext
                     ) throws EvaluationException {
 
-
         EvaluationContext context = new EvaluationContext();
 
-        SqlQueryBuilder q = new SqlQueryBuilder();
-        q.append("(select e.patient_id, e.encounter_datetime as lastSeen");
-        q.append(" from encounter e");
-        q.append(" inner join (");
-        q.append(" select patient_id, max(encounter_datetime) lastSeen ");
-        q.append(" from encounter");
-        q.append(" where voided = 0 and visit_id in ( select visit_id from visit where date_stopped is null and voided = 0)");
-        q.append(" group by patient_id");
-        q.append(" ) b on e.patient_id = b.patient_id and  e.encounter_datetime = b.lastSeen");
-        q.append(" where e.voided = 0 and e.visit_id in");
-        q.append(" ( select visit_id from visit where date_stopped is null and voided = 0)");
-        if (location != null) {
-            q.append(" and e.location_id = :location").addParameter("location", location);
+        // use the closest visit location associated with the session location if the location param is not specified
+        if (locationId == null) {
+            Location location = adtService.getLocationThatSupportsVisits(uiSessionContext.getSessionLocation());
+            if (location != null) {
+                locationId = location.getId();
+            }
         }
-        q.append(" )");
-        q.append(" union");
-        q.append(" (select v.patient_id, v.date_started as lastSeen");
-        q.append(" from visit v");
-        q.append(" where v.date_stopped is null and v.voided = 0 ");
-        q.append(" and v.visit_id not in");
-        q.append(" (select e.visit_id from encounter e where e.patient_id = v.patient_id and e.visit_id is not null and e.voided = 0))");
-        q.append(" order by lastSeen desc");
+
+        SqlQueryBuilder q = new SqlQueryBuilder();
+        q.append("select distinct patient_id from visit");
+        q.append(" where date_stopped is null and voided = 0");
+        if (locationId != null) {
+            q.append(" and location_id = :location").addParameter("location", locationId);
+        }
 
         DbSessionFactory dbSessionFactory = Context.getRegisteredComponents(DbSessionFactory.class).get(0);
         List<Object[]> resultSet = q.evaluateToList(dbSessionFactory, context);
@@ -67,7 +61,7 @@ public class ActiveVisitsListPageController {
         }
 
         model.addAttribute("locale", uiSessionContext.getLocale());
-        model.addAttribute("lastLocation", location);
+        model.addAttribute("lastLocation", locationId);
         model.addAttribute("activeVisitsCohort", activeVisitsCohort);
         model.addAttribute("dashboardUrl", coreAppsProperties.getDashboardUrl());
         model.put("privilegePatientDashboard", CoreAppsConstants.PRIVILEGE_PATIENT_DASHBOARD);  // used to determine if we display links to patient dashboard)
