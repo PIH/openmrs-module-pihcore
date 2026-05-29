cd00658cb9a0453b660d9297e2fe0f8aee673123
Cosmin <cioan@pih.org>
2026-05-29 08:39:48 -0400

    SL-1292: add inborn status to dashboard widget (#660)
    * SL-1292: add inborn status to dashboard widget

--- omod/src/main/java/org/openmrs/module/pihcore/fragment/controller/dashboardwidgets/PatientLocationFragmentController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/fragment/controller/dashboardwidgets/PatientLocationFragmentController.java
@@ -2,7 +2,12 @@ package org.openmrs.module.pihcore.fragment.controller.dashboardwidgets;
 
 
 import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.VisitAttribute;
+import org.openmrs.VisitAttributeType;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.appframework.domain.AppDescriptor;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.adt.AdtService;
@@ -19,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PatientLocationFragmentController {
+    private static String BORN_DURING_VISIT_ATTRIBUTE_TYPE = "86f716fc-5e26-4eb1-9484-46370cff28f0";
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     public void controller(@SpringBean("patientService") PatientService patientService,
@@ -59,5 +65,56 @@ public class PatientLocationFragmentController {
         model.put("patientStatus", inpatientLocation.get("patientStatus"));
         model.put("inpatientLocation", inpatientLocation.get("currentInpatientLocation"));
         model.put("queueName", inpatientLocation.get("queueName"));
+        model.put("activeVisitUuid", activeVisit != null ? activeVisit.getVisit().getUuid() : null);
+        model.put("isBornDuringVisit", getBornDuringVisitValue(activeVisit));
+        model.put("visitAttributeUuid", getVisitAttributeUuid(activeVisit));
+        model.put("bornDuringVisitAttributeType", BORN_DURING_VISIT_ATTRIBUTE_TYPE);
     }
+
+    /**
+     * Gets the UUID of the "born during visit" visit attribute
+     *
+     * @param activeVisit the active visit wrapper, may be null
+     * @return the visit attribute UUID, or null if not found
+     */
+    private String getVisitAttributeUuid(VisitDomainWrapper activeVisit) {
+        if (activeVisit != null && activeVisit.getVisit() != null) {
+            Visit visit = activeVisit.getVisit();
+            VisitAttributeType visitAttributeType = Context.getVisitService().getVisitAttributeTypeByUuid(BORN_DURING_VISIT_ATTRIBUTE_TYPE);
+            if (visitAttributeType != null) {
+                for (VisitAttribute attribute : visit.getActiveAttributes()) {
+                    if (attribute.getAttributeType().equals(visitAttributeType)) {
+                        return attribute.getUuid();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets the boolean value of the "born during visit" attribute
+     *
+     * @param activeVisit the active visit wrapper, may be null
+     * @return true if born during visit, false otherwise
+     */
+    private boolean getBornDuringVisitValue(VisitDomainWrapper activeVisit) {
+        if (activeVisit != null && activeVisit.getVisit() != null) {
+            Visit visit = activeVisit.getVisit();
+            VisitAttributeType visitAttributeType = Context.getVisitService().getVisitAttributeTypeByUuid(BORN_DURING_VISIT_ATTRIBUTE_TYPE);
+            if (visitAttributeType != null) {
+                for (VisitAttribute attribute : visit.getActiveAttributes()) {
+                    if (attribute.getAttributeType().equals(visitAttributeType)) {
+                        Object value = attribute.getValue();
+                        if (value != null && value instanceof Boolean) {
+                            return (Boolean)value;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
