f6c3ec112181726c3db8d10c072d72ae116366ef
Michael Seaton <mseaton@pih.org>
2026-07-30 09:14:06 -0400

    UHM-9288 - Add component to control which lab app to use

--- omod/src/main/java/org/openmrs/module/pihcore/page/controller/router/LabRouterPageController.java
+++ omod/src/main/java/org/openmrs/module/pihcore/page/controller/router/LabRouterPageController.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.pihcore.page.controller.router;
 
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.Patient;
 import org.openmrs.module.pihcore.config.Components;
 import org.openmrs.module.pihcore.config.Config;
@@ -13,7 +14,9 @@ public class LabRouterPageController {
 
     public Redirect controller(HttpServletRequest request,
                                @SpringBean("config") Config config,
-                               @RequestParam(value = "patient", required = false) Patient patient) {
+                               @RequestParam(value = "patient", required = false) Patient patient,
+                               @RequestParam(value = "returnUrl", required = false) String returnUrl,
+                               @RequestParam(value = "dashboard", required = false) String dashboard) {
 
         String redirectUrl;
         boolean usePihAppsLabs = config.isComponentEnabled(Components.LABS_USING_PIH_APPS);
@@ -23,7 +26,14 @@ public class LabRouterPageController {
                 redirectUrl = "pihapps/labs/patientLabResults.page?patient=" + patientUuid;
             }
             else {
-                redirectUrl = "owa/labworkflow/index.html?patient=" + patientUuid + "&returnUrl=" + "/coreapps/clinicianfacing/patient.page?patientId=" + patientUuid + "#/LabResults";
+                if (StringUtils.isBlank(returnUrl)) {
+                    returnUrl = "/coreapps/clinicianfacing/patient.page?patientId=" + patientUuid;
+                    if (StringUtils.isNotBlank(dashboard)) {
+                        returnUrl += "&dashboard=" + dashboard;
+                    }
+                    returnUrl += "#/LabResults";
+                }
+                redirectUrl = "owa/labworkflow/index.html?patient=" + patientUuid + "&returnUrl=" + returnUrl;
             }
         }
         else {
